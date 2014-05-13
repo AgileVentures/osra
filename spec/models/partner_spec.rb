cb09c4aa4f89c8121e6acf21feb0d9f52fa00f2e
Motasem Salem <motasem@gmail.com>
2014-05-13 09:28:46 -0400

    province specs

--- spec/models/partner_spec.rb
+++ spec/models/partner_spec.rb
@@ -9,17 +9,11 @@ describe Partner do
 
   it "should have a valid name and province" do
     province = Province.create(:code => 11)
-
     Partner.create(:name => "Partner One", :province => province).should be_valid
   end
 
   it "should require a province" do
     Partner.create(:name => "Partner One").should_not be_valid
-  end
-
-  it "should reject an invalid province" do
-    province = Province.create(:code => 99)
-    Partner.create(:name => "Partner One", :province => province).should_not be_valid
   end
 
 end
--- spec/models/province_spec.rb
+++ spec/models/province_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Province do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should reject a province with an invalid code" do
+    Province.new(:code => 99).should_not be_valid
+  end
+
+  it "should accept provices with valid codes" do
+    [11,12,13,14,15,16,17,18,19,29].each do |c|
+      Province.new(:code => c).should be_valid
+    end
+  end
+
+
+
 end
