96fc2988069c158060ba26dab99525a4a0410c4c
Sampriti Panda <sampdingo@hotmail.com>
2014-05-13 19:21:40 +0530

    Added Provinces to Seeds

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,6 +1,6 @@
 class Province < ActiveRecord::Base
 
-  PROVINCE_CODES = [11,12,13,14,15,16,17,18,19,29]
+  PROVINCE_CODES = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 29]
 
   validates_presence_of :name
   validates_inclusion_of :code, in: PROVINCE_CODES
