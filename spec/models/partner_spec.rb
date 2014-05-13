fee7ae65a3fd84053ad656dd4b3f7b6825d8a994
Sampriti Panda <sampdingo@hotmail.com>
2014-05-13 19:10:58 +0530

    Specs and Validations for Partner and Province

--- spec/models/partner_spec.rb
+++ spec/models/partner_spec.rb
@@ -1,19 +1,17 @@
 require 'spec_helper'
 
 describe Partner do
+  it "should not be valid without a name" do
+    province = Province.create(:code => 11)
+    Partner.new(:province => province).should be_valid
+  end
 
-
-  it "should require a name" do
-    Partner.create().should_not be_valid
+  it "should not be valid without a province" do
+    Partner.new(:name => "Partner One").should_not be_valid
   end
 
   it "should have a valid name and province" do
     province = Province.create(:code => 11)
-    Partner.create(:name => "Partner One", :province => province).should be_valid
+    Partner.new(:name => "Partner One", :province => province).should be_valid
   end
-
-  it "should require a province" do
-    Partner.create(:name => "Partner One").should_not be_valid
-  end
-
 end
