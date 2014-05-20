f80f30432d348deaf03d914451abff5a799b9e13
Motasem Salem <motasem@gmail.com>
2014-05-20 07:13:44 -0400

    Testing for province uniqueness

--- spec/models/province_spec.rb
+++ spec/models/province_spec.rb
@@ -9,9 +9,20 @@ describe Province do
     Province.new(:name => "Damascus", :code => 99).should_not be_valid
   end
 
-  it "should accept provices with names and valid codes" do
+  it "should accept provinces with names and valid codes" do
     [11,12,13,14,15,16,17,18,19,29].each do |c|
       Province.new(:name => "Damascus", :code => c).should be_valid
     end
   end
+
+  it "name should be unique" do
+    Province.create(:name => "Damascus", :code => 11)
+    Province.create(:name => "Damascus", :code => 12).should_not be_valid
+  end
+
+  it "code should be unique" do
+    Province.create(:name => "Damascus", :code => 11)
+    Province.create(:name => "Aleppo", :code => 11).should_not be_valid
+  end
+
 end
