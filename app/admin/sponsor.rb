1723b9640a2505e01bbe74204e7fb4dfb095950a
Motasem Salem <motasem@gmail.com>
2014-07-30 09:13:43 -0400

    Created Sponsor new / edit form

--- app/admin/sponsor.rb
+++ app/admin/sponsor.rb
@@ -13,5 +13,22 @@ ActiveAdmin.register Sponsor do
   #  permitted << :other if resource.something?
   #  permitted
   # end
-  
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :country, as: :string
+      f.input :address
+      f.input :email
+      f.input :contact1
+      f.input :contact2
+      f.input :additional_info
+      f.input :sponsorship_start_date, as: :datepicker
+      f.input :status
+    end
+    f.actions
+  end
+
+  permit_params :name, :country, :address, :email, :contact1, :contact2, :additional_info, :sponsorship_start_date, :status
+
 end
--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -3,6 +3,7 @@ module DateValidator
 
   def validate_date_not_in_future
   	# clumsy hack
+  	#debugger
    	if self.is_a?(Partner)
     	self.errors.add(:partnership_start_date, "is not valid (should not be in the future)") unless (self.partnership_start_date||Date.today) <= Date.today
     elsif self.is_a?(Sponsor)
