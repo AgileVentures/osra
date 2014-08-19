c3f69f8226cff03e56622a9c917b8baa0c86c1ff
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2014-08-19 21:29:50 +0300

    Add SponsorType model spec using factory. - nikita

--- spec/models/sponsor_type_spec.rb
+++ spec/models/sponsor_type_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 describe SponsorType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:sponsor_type) { FactoryGirl.build_stubbed(:sponsor_type) }
+
+  subject { sponsor_type }
+
+  it { should be_valid }
+
+  describe 'validations' do
+    context 'when name is missing' do
+      before { sponsor_type.name = '' }
+      it { should_not be_valid }
+    end
+
+    context 'when code is missing' do
+      before { sponsor_type.code = '' }
+      it { should_not be_valid }
+    end
+
+    context 'when name is not unique' do
+      before { FactoryGirl.create(:sponsor_type, name: sponsor_type.name) }
+      it { should_not be_valid }
+      end
+
+    context 'when code is not unique' do
+      before { FactoryGirl.create(:sponsor_type, code: sponsor_type.code) }
+      it { should_not be_valid }
+    end
+  end
 end
