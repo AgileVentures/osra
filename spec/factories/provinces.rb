c702f62c243decbf3ecaa2b31b7d6b7c9279e354
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2014-08-23 00:22:44 +0300

    Province factory uses paired real province names & codes

--- spec/factories/provinces.rb
+++ spec/factories/provinces.rb
@@ -1,9 +1,22 @@
 FactoryGirl.define do
   factory :province do
-    sequence(:name) { |n| "Name #{n}" }
-    sequence(:code, 0) do |n|
-      valid_codes = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 29]
-      valid_codes[n]
-    end
+    provinces = [
+        ['Damascus & Rif Dimashq', 11],
+        ['Aleppo', 12],
+        ['Homs', 13],
+        ['Hama', 14],
+        ['Latakia', 15],
+        ['Deir Al-Zor', 16],
+        ['Daraa', 17],
+        ['Idlib', 18],
+        ['Ar Raqqah', 19],
+        ['Al Ḥasakah', 20],
+        ['Tartous', 21],
+        ['Al-Suwayada', 22],
+        ['Al-Quneitera', 23],
+        ['Outside Syria', 29]
+  ]
+    sequence(:name, 0) { |n| provinces[n][0] }
+    sequence(:code, 0) { |n| provinces[n][1] }
   end
 end
