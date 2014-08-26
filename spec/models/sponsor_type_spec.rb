17620bde3c9ab3fd9b8a9f3d603594dac6aba2de
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2014-08-27 00:14:50 +0300

    Code review: remove province factory; refactor sponsor_type_spec.rb & update to RSpec3.0 syntax

--- spec/models/sponsor_type_spec.rb
+++ spec/models/sponsor_type_spec.rb
@@ -1,31 +1,29 @@
 require 'rails_helper'
 
 describe SponsorType, type: :model do
-  let(:sponsor_type) { build_stubbed(:sponsor_type) }
-
-  subject { sponsor_type }
+  subject(:sponsor_type) { build_stubbed(:sponsor_type) }
 
   it { should be_valid }
 
   describe 'validations' do
     context 'when name is missing' do
       before { sponsor_type.name = '' }
-      it { should_not be_valid }
+      it { is_expected.not_to be_valid }
     end
 
     context 'when code is missing' do
       before { sponsor_type.code = '' }
-      it { should_not be_valid }
+      it { is_expected.not_to be_valid }
     end
 
     context 'when name is not unique' do
       before { create(:sponsor_type, name: sponsor_type.name) }
-      it { should_not be_valid }
+      it { is_expected.not_to be_valid }
       end
 
     context 'when code is not unique' do
       before { create(:sponsor_type, code: sponsor_type.code) }
-      it { should_not be_valid }
+      it { is_expected.not_to be_valid }
     end
   end
 end
