093beae6364240a23ac9ecb01ed1cb7a1e1a6377
Motasem Salem <motasem@gmail.com>
2014-08-30 08:12:11 +0300

    Switched Address spec to shoulda matchers

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -2,27 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Address, :type => :model do
 
-  before(:each) do
-    @address = Address.new
-  end
+  it {should validate_presence_of(:province)}
 
-  it "should have a valid province" do
-    invalid
-    expect(@address.errors[:province].size).to eq(1)
+  it {should validate_presence_of(:city)}
 
-  end
+  it {should validate_presence_of(:neighborhood)}
 
-  it "should have a valid city" do
-    invalid
-    expect(@address.errors[:city].size).to eq(1)
-  end
-
-  it "should have a valid neighborhood" do
-    invalid
-    expect(@address.errors[:neighborhood].size).to eq(1)
-  end
-
-  def invalid
-    expect(@address).to be_invalid
-  end
 end
