b853ce552397beef2c2bd9d709f91d42d2dde4a9
Motasem Salem <motasem@gmail.com>
2014-08-30 08:08:12 +0300

    Switched to shoulda matchers

--- app/models/orphan.rb
+++ app/models/orphan.rb
@@ -4,14 +4,14 @@ class Orphan < ActiveRecord::Base
 
   validates :name, presence: true
   validates :father_name, presence: true
-  validates :father_is_martyr, inclusion: {in: [true, false] }
+  validates :father_is_martyr, inclusion: {in: [true, false] }, exclusion: { in: [nil]}
   validates :father_date_of_death, presence: true
   validates :mother_name, presence: true
-  validates :mother_alive, inclusion: {in: [true, false] }
+  validates :mother_alive, inclusion: {in: [true, false] }, exclusion: { in: [nil]}
   validates :date_of_birth, presence: true
-  validates :gender, presence: true
+  validates :gender, presence: true, inclusion: {in: %w(Male Female) } # TODO: DRY list of allowed values
   validates :contact_number, presence: true
-  validates :sponsored_by_another_org, inclusion: {in: [true, false] }
+  validates :sponsored_by_another_org, inclusion: {in: [true, false] }, exclusion: { in: [nil]}
   validates :minor_siblings_count, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :original_address, presence: true
   validates :current_address, presence: true
--- spec/models/orphan_spec.rb
+++ spec/models/orphan_spec.rb
@@ -2,137 +2,56 @@ require 'rails_helper'
 
 describe Orphan, type: :model do
 
-  before(:each) do
-    @orphan = Orphan.new
-  end
+  it { should validate_presence_of(:name) }
 
-  it "should have a valid name" do
-    invalid
-    expect(@orphan.errors[:name].size).to eq(1)
-  end
+  it { should validate_presence_of(:father_name) }
 
-  it "should have a valid father name" do
-    invalid
-    expect(@orphan.errors[:father_name].size).to eq(1)
-  end
+  it { should validate_presence_of(:father_date_of_death) }
 
-  it "should have a value for father is martyr?" do
-    invalid
-    expect(@orphan.errors[:father_is_martyr].size).to eq(1)
-  end
+  it { should allow_value(Date.today - 5,'2011-03-15').for(:father_date_of_death) }
 
-  it "should allow true value for father is martyr?" do
-    @orphan.father_is_martyr = true
-    expect(@orphan.errors[:father_is_martyr].size).to eq(0)
-  end
+  it { should_not allow_value(Date.today + 5).for(:father_date_of_death) }
 
-  it "should allow false value for father is martyr?" do
-    @orphan.father_is_martyr = false
-    expect(@orphan.errors[:father_is_martyr].size).to eq(0)
-  end
+  it { should_not allow_value(7).for(:father_date_of_death) }
 
-  it "should have a valid father date of death" do
-    invalid
-    expect(@orphan.errors[:father_date_of_death].size).to be >= 1
-  end
+  it { should_not allow_value('this is not a date value').for(:father_date_of_death) }
 
-  it "should not allow a value for father date of death that is not a date" do
-    @orphan.father_date_of_death = 3
-    invalid
-    expect(@orphan.errors[:father_date_of_death].size).to eq(1)
-  end
+  it { should_not allow_value(nil).for(:father_is_martyr) }
 
-  it "should not allow a future date for father date of death" do
-    @orphan.father_date_of_death = '2050-01-01'
-    invalid
-    expect(@orphan.errors[:father_date_of_death].size).to eq(1)
-  end
+  it { should allow_value(true, false).for(:father_is_martyr) }
 
-  it "should allow a date value for father date of death" do
-    @orphan.father_date_of_death = Date.today
-    expect(@orphan.errors[:father_date_of_death].size).to eq(0)
-  end
+  it { should validate_presence_of(:mother_name) }
 
-  it "should allow a valid string value for father date of death" do
-    @orphan.father_date_of_death = '2014-08-13'
-    expect(@orphan.errors[:father_date_of_death].size).to eq(0)
-  end
+  it { should_not allow_value(nil).for(:mother_alive) }
 
-  it "should have a valid mother name" do
-    invalid
-    expect(@orphan.errors[:mother_name].size).to eq(1)
-  end
+  it { should allow_value(true, false).for(:mother_alive) }
 
-  it "should allow true value for mother alive?" do
-    @orphan.mother_alive = true
-    expect(@orphan.errors[:mother_alive].size).to eq(0)
-  end
+  it { should allow_value(Date.today - 5,'2011-03-15').for(:date_of_birth) }
 
-  it "should allow false value for mother alive?" do
-    @orphan.mother_alive = false
-    expect(@orphan.errors[:mother_alive].size).to eq(0)
-  end
+  it { should_not allow_value(Date.today + 5).for(:date_of_birth) }
 
-  it "should have a valid date of birth" do
-    invalid
-    expect(@orphan.errors[:date_of_birth].size).to be >= 1
-  end
+  it { should_not allow_value(7).for(:date_of_birth) }
 
-  it "should not allow a future date for date of birth" do
-    @orphan.date_of_birth = '2050-01-01'
-    invalid
-    expect(@orphan.errors[:date_of_birth].size).to eq(1)
-  end
+  it { should_not allow_value('this is not a date value').for(:date_of_birth) }
 
-  it "should have a valid gender" do
-    invalid
-    expect(@orphan.errors[:gender].size).to eq(1)
-  end
+  it { should validate_presence_of(:gender) }
 
-  it "should have a valid contact number" do
-    invalid
-    expect(@orphan.errors[:contact_number].size).to eq(1)
-  end
+  it { should allow_value('Male', 'Female').for(:gender) }
 
-  it "should allow true value for sponsored by another org?" do
-    @orphan.sponsored_by_another_org = true
-    expect(@orphan.errors[:sponsored_by_another_org].size).to eq(0)
-  end
+  it { should_not allow_value('male').for(:gender) }
 
-  it "should allow false value for sponsored by another org?" do
-    @orphan.sponsored_by_another_org = false
-    expect(@orphan.errors[:sponsored_by_another_org].size).to eq(0)
-  end
+  it { should_not allow_value('female').for(:gender) }
 
-  it "should have a valid minor siblings count" do
-    invalid
-    expect(@orphan.errors[:minor_siblings_count].size).to be >= 1
-  end
+  it { should validate_presence_of(:contact_number) }
 
-  it "should not allow a float as minor siblings count" do
-    @orphan.minor_siblings_count = 1.1
-    invalid
-    expect(@orphan.errors[:minor_siblings_count].size).to eq(1)
-  end
+  it { should_not allow_value(nil).for(:sponsored_by_another_org) }
 
-  it "should not allow a negative number as minor siblings count" do
-    @orphan.minor_siblings_count = -2
-    invalid
-    expect(@orphan.errors[:minor_siblings_count].size).to eq(1)
-  end
+  it { should allow_value(true, false).for(:sponsored_by_another_org) }
 
-  it "should have a valid original address" do
-    invalid
-    expect(@orphan.errors[:original_address].size).to eq(1)
-  end
+  it { should validate_numericality_of(:minor_siblings_count).only_integer.is_greater_than_or_equal_to(0) }
 
-  it "should have a valid current address" do
-    invalid
-    expect(@orphan.errors[:current_address].size).to eq(1)
-  end
+  it { should validate_presence_of(:original_address) }
 
-  def invalid
-    expect(@orphan).to be_invalid
-  end
+  it { should validate_presence_of(:current_address) }
 
 end
