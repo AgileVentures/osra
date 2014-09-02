06e378bee9b0f8548d78004bea121cc70d346b90
Sampriti Panda <sampdingo@hotmail.com>
2014-09-02 21:30:44 +0530

    Add DatabaseCleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,4 +44,15 @@ RSpec.configure do |config|
 
   # Use FactoryGirl methods without prefacing them with 'FactoryGirl.'
   config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
