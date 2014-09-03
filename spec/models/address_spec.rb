7116bc74bb61e142349408f739ec5198454458df
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2014-09-03 21:37:41 +0300

    Refactor address_spec; delete empty spec files

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Address, :type => :model do
+describe Address, :type => :model do
+
+  it 'should have a valid factory' do
+    expect(build_stubbed :address).to be_valid
+  end
 
   it { is_expected.to validate_presence_of :province }
 
@@ -8,4 +12,5 @@ RSpec.describe Address, :type => :model do
 
   it { is_expected.to validate_presence_of :neighborhood }
 
+  it { is_expected.to belong_to :province }
 end
