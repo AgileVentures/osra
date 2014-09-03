500b994cd1f12ced1f99a6053aae21ae4b321fb6
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2014-09-03 21:50:12 +0300

    Refactor province_spec

--- spec/models/province_spec.rb
+++ spec/models/province_spec.rb
@@ -2,36 +2,13 @@ require 'rails_helper'
 
 describe Province, type: :model do
 
-  it "should not be valid without a name" do
-    expect(Province.new(:code => 11)).to be_invalid
+  it 'should have a valid factory' do
+    expect(build_stubbed :province).to be_valid
   end
 
-  it "should reject a province with an invalid code" do
-    expect(Province.new(:name => "Damascus", :code => 99)).to be_invalid
-  end
-
-  it "should accept provinces with names and valid codes" do
-    #   DatabaseCleaner.clean
-    Province.destroy_all
-    [11, 12, 13, 14, 15, 16, 17, 18, 19, 29].each do |c|
-      expect(Province.new(:name => "Damascus#{c}", :code => c)).to be_valid
-    end
-  end
-
-  it "name should be unique" do
-    Province.create(:name => "Damascus", :code => 11)
-    expect(Province.new(:name => "Damascus", :code => 12)).to be_invalid
-  end
-
-  it "code should be unique" do
-    Province.create(:name => "Damascus", :code => 11)
-    expect(Province.new(:name => "Aleppo", :code => 11)).to be_invalid
-  end
-
-  after(:each) do
-    Province.all.each do |p|
-      p.destroy!
-    end
-  end
-
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_uniqueness_of :name }
+  it { is_expected.to validate_presence_of :code }
+  it { is_expected.to validate_uniqueness_of :code }
+  it { is_expected.to ensure_inclusion_of(:code).in_array Province::PROVINCE_CODES }
 end
