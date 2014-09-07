8d02c3934d8e50cf9e64c6511356b8fd2cede0b3
Craig Ferry <PurityControl@users.noreply.github.com>
2014-09-07 21:25:20 +0000

    added all optional field to orphan form model
    part of PT story https://www.pivotaltracker.com/story/show/76239618

--- app/admin/orphan.rb
+++ app/admin/orphan.rb
@@ -13,10 +13,16 @@ ActiveAdmin.register Orphan do
   #   permitted << :other if resource.something?
   #   permitted
   # end
-  permit_params :name, :father_name, :father_is_martyr, :father_date_of_death, 
-                :mother_name, :mother_alive, :date_of_birth, :gender,
-                :contact_number, :sponsored_by_another_org, 
-                :minor_siblings_count, original_address_attributes: [ :city,
+  permit_params :name, :father_name, :father_is_martyr, :father_occupation, 
+                :father_place_of_death, :father_cause_of_death, 
+                :father_date_of_death, :mother_name, :mother_alive, 
+                :date_of_birth, :gender, :health_status, :schooling_status,
+                :goes_to_school, :guardian_name, :guardian_relationship, 
+                :guardian_id, :contact_number, :alt_contact_number, 
+                :sponsored_by_another_org, :another_org_sponsorship_details, 
+                :minor_siblings_count, :sponsored_minor_siblings_count, 
+                :comments,
+                original_address_attributes: [ :city,
                 :province_id, :neighborhood, :street, :details], 
                 current_address_attributes: [ :city,
                 :province_id, :neighborhood, :street, :details]
@@ -27,14 +33,27 @@ ActiveAdmin.register Orphan do
       f.input :name
       f.input :father_name
       f.input :father_is_martyr
+      f.input :father_occupation
+      f.input :father_place_of_death
+      f.input :father_cause_of_death
       f.input :father_date_of_death
       f.input :mother_name
       f.input :mother_alive
       f.input :date_of_birth
       f.input :gender
+      f.input :health_status
+      f.input :schooling_status
+      f.input :goes_to_school
+      f.input :guardian_name
+      f.input :guardian_relationship
+      f.input :guardian_id
       f.input :contact_number
+      f.input :alt_contact_number
       f.input :sponsored_by_another_org
+      f.input :another_org_sponsorship_details
       f.input :minor_siblings_count
+      f.input :sponsored_minor_siblings_count
+      f.input :comments
     end
    
 
