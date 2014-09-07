2a5acc030dce90278fe8c605707d9ac6d0d79379
betasve <svetoslav@metaideas.bg>
2014-09-07 17:07:30 +0300

    add schema after migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140908035646) do
+ActiveRecord::Schema.define(version: 20140905090606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,9 +68,22 @@ ActiveRecord::Schema.define(version: 20140908035646) do
     t.datetime "updated_at"
   end
 
+<<<<<<< HEAD
   create_table "orphan_statuses", force: true do |t|
     t.integer "code"
     t.string  "name"
+=======
+  create_table "organizations", force: true do |t|
+    t.integer  "code"
+    t.string   "name"
+    t.string   "country"
+    t.string   "region"
+    t.date     "start_date"
+    t.integer  "status_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "sequential_id"
+>>>>>>> add schema after migrations
   end
 
   create_table "orphans", force: true do |t|
