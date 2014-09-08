c7ab5555e627bd0592d0d57170cdbb69e46f3932
Sampriti Panda <sampdingo@hotmail.com>
2014-09-08 14:20:43 +0530

    Update specs with latest validation changes in OrphanList model

--- spec/models/orphan_list_spec.rb
+++ spec/models/orphan_list_spec.rb
@@ -1,12 +1,9 @@
 require 'rails_helper'
 
 describe OrphanList, type: :model do
-  it { is_expected.to validate_presence_of :osra_num }
   it { is_expected.to validate_presence_of :partner }
   it { is_expected.to validate_presence_of :orphan_count }
   it { is_expected.to validate_presence_of :file }
-
-  it { is_expected.to validate_uniqueness_of :osra_num }
 
   it { is_expected.to belong_to :partner }
 end
