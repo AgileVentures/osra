8af075643adcdb7ecd285e40a1d6f9c25d59b573
Sampriti Panda <sampdingo@hotmail.com>
2014-09-08 14:09:11 +0530

    Added Index and Download action.

--- app/admin/orphan_list.rb
+++ app/admin/orphan_list.rb
@@ -1,4 +1,25 @@
 ActiveAdmin.register OrphanList do
+  actions :index, :new, :create
+
+  index do
+    column :osra_num
+    column :partner, sortable: :partner_id do |orphan_list|
+      link_to orphan_list.partner.name, admin_partner_path(orphan_list.partner)
+    end
+    column :import_date, sortable: :created_at do |orphan_list|
+      orphan_list.created_at.to_date
+    end
+    column :orphan_count
+    column do |orphan_list|
+      link_to 'Download', download_admin_orphan_list_path(orphan_list)
+    end
+  end
+
+  filter :partner
+  filter :osra_num
+  filter :created_at, label: 'Import Date'
+  filter :orphan_count
+
   form do |f|
     f.inputs do
       f.input :partner
@@ -6,6 +27,13 @@ ActiveAdmin.register OrphanList do
       f.input :file, as: :file
     end
     f.actions
+  end
+
+  member_action :download, method: :get do
+    @orphan_list = OrphanList.find(params[:id])
+    send_data(@orphan_list.file,
+              type: 'application/vnd.ms-excel',
+              filename: "#{@orphan_list.osra_num}.xls")
   end
 
   controller do
