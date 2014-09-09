36805cc499c5262f146b9101e2381bb499d3c95d
Motasem Salem <motasem@gmail.com>
2014-09-09 05:49:39 +0300

    removed unneeded comments

--- app/admin/orphan.rb
+++ app/admin/orphan.rb
@@ -1,19 +1,5 @@
 ActiveAdmin.register Orphan do
 
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
   actions :all, except: [:destroy]
 
   permit_params :name, :father_name, :father_is_martyr, :father_occupation,
