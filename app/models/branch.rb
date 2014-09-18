5a8903eea1d40f6ac01405bdb925ba50a62b6189
Craig Ferry <PurityControl@users.noreply.github.com>
2014-09-18 15:12:31 +0000

    addendum to organisation crud story fixing branch code should be unique
    part of PT story https://www.pivotaltracker.com/story/show/77974308

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,7 +1,8 @@
 class Branch < ActiveRecord::Base
 
   validates :name, presence: true
-  validates :code, presence: true, numericality: { only_integer: true}, inclusion: 0..99
+  validates :code, presence: true, uniqueness: true,
+            numericality: { only_integer: true}, inclusion: 0..99
 
   has_many :sponsors
 
