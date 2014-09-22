09a129d2ec08050155dd7d5279fe0e39b4129465
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2014-09-22 16:44:19 +0300

    Improve province factory to allow infinite cycling over 14 valid province objects

--- spec/models/province_spec.rb
+++ spec/models/province_spec.rb
@@ -6,6 +6,10 @@ describe Province, type: :model do
     expect(build_stubbed :province).to be_valid
   end
 
+  it 'should be able to call the factory many times' do
+    25.times { build_stubbed :province }
+  end
+
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_uniqueness_of :name }
   it { is_expected.to validate_presence_of :code }
