4ba5a238c5a808fa5d5e0b63991b657705ffa25b
Motasem Salem <motasem@gmail.com>
2014-09-25 12:20:00 -0400

    Code review on orphan list factories and specs

--- spec/factories/orphan_lists.rb
+++ spec/factories/orphan_lists.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     # Does the following line work correctly on Windows?
     spreadsheet { Rack::Test::UploadedFile.new 'spec/files/empty.xlsx', 'application/vnd.ms-excel' }
     orphan_count { (0..50).to_a.sample }
-    association :partner, factory: :partner
+    partner
   end
 end
 
--- spec/models/orphan_list_spec.rb
+++ spec/models/orphan_list_spec.rb
@@ -2,26 +2,22 @@ require 'rails_helper'
 
 describe OrphanList, type: :model do
 
-  let(:orphan_list) { build_stubbed(:orphan_list) }
-
-  it 'should have a valid factory' do
-    orphan_list.partner.status = Status.find_or_create_by(name: 'Active', code: 1)
-    expect(orphan_list).to be_valid
-  end
+  let(:active_status) { build_stubbed :status, name: 'Active', code: 1 }
+  let(:inactive_status) { build_stubbed :status, name: 'Inactive', code: 2 }
+  let(:active_partner) { build_stubbed :partner, status: active_status }
+  let(:inactive_partner) { build_stubbed :partner, status: inactive_status }
 
   it { is_expected.to validate_presence_of :partner }
   it { is_expected.to validate_presence_of :orphan_count }
   it { is_expected.to validate_presence_of :spreadsheet }
+  it { is_expected.to belong_to :partner }
 
   it 'should not belong to a non active partner' do
-    orphan_list.partner.status = Status.find_or_create_by(name: 'Inactive', code: 2)
-    expect(orphan_list).to_not be_valid
+    expect(build_stubbed :orphan_list, partner: inactive_partner).not_to be_valid
   end
 
-  it 'should belong to an active partner' do
-    orphan_list.partner.status = Status.find_or_create_by(name: 'Active', code: 1)
-    expect(orphan_list).to be_valid
+  it 'should have a valid factory and belong to an active partner' do
+    expect(build_stubbed :orphan_list, partner: active_partner).to be_valid
   end
 
-  it { is_expected.to belong_to :partner }
 end
