bf6ecb2aa0903fad3af5400ba97152f0d8febb1f
Motasem Salem <motasem@gmail.com>
2014-09-25 12:07:31 -0400

    DRYed orphan_list specs using let

--- spec/models/orphan_list_spec.rb
+++ spec/models/orphan_list_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe OrphanList, type: :model do
 
+  let(:orphan_list) { build_stubbed(:orphan_list) }
+
   it 'should have a valid factory' do
-    orphan_list = build_stubbed :orphan_list
     orphan_list.partner.status = Status.find_or_create_by(name: 'Active', code: 1)
     expect(orphan_list).to be_valid
   end
@@ -13,13 +14,11 @@ describe OrphanList, type: :model do
   it { is_expected.to validate_presence_of :spreadsheet }
 
   it 'should not belong to a non active partner' do
-    orphan_list = build_stubbed :orphan_list
     orphan_list.partner.status = Status.find_or_create_by(name: 'Inactive', code: 2)
     expect(orphan_list).to_not be_valid
   end
 
   it 'should belong to an active partner' do
-    orphan_list = build_stubbed :orphan_list
     orphan_list.partner.status = Status.find_or_create_by(name: 'Active', code: 1)
     expect(orphan_list).to be_valid
   end
