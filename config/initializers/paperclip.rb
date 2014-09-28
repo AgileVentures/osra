969a5081a1ed5f8fac11f7f97d8b5872b15584a2
Motasem Salem <motasem@gmail.com>
2014-09-28 21:39:18 +0300

    One more trial. Use a paperclip initializer with hardcoded mime types

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,2 @@
+Paperclip.options[:content_type_mappings] = { xlsx: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                                              xls:  'application/vnd.ms-excel'}
