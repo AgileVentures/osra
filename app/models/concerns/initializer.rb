4e1653d684ed057d15f931a55cdcf32ad4527ca7
Craig Ferry <PurityControl@users.noreply.github.com>
2014-10-01 20:33:06 +0000

    move model-specific initializer methods from initializer.rb to models
    OSRA-152 #Code-Review

--- app/models/concerns/initializer.rb
+++ app/models/concerns/initializer.rb
@@ -7,12 +7,4 @@ module Initializer
   def default_start_date_to_today
     self.start_date ||= Date.current
   end
-
-  def default_orphan_status_to_active
-    self.orphan_status ||= OrphanStatus.find_by_name 'Active'
-  end
-
-  def default_sponsorship_status_to_unsponsored
-    self.orphan_sponsorship_status ||= OrphanSponsorshipStatus.find_by_name 'Unsponsored'
-  end
 end
