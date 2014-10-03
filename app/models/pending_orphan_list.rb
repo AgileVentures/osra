def700956f27fc8618c543ea6e6cd1d8b673f9f0
Motasem Salem <motasem@gmail.com>
2014-10-04 00:57:19 +0300

    added pending models

---
+++ app/models/pending_orphan_list.rb
@@ -0,0 +1,8 @@
+class PendingOrphanList < ActiveRecord::Base
+  has_attached_file :spreadsheet
+
+  validates_attachment :spreadsheet, presence: true,
+                       file_name: {matches: [/xls\Z/, /xlsx\Z/]}
+
+  has_many :pending_orphans
+end
