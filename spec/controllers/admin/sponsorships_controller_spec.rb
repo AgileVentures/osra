c6ca8a15c07e6263718efd7d790dc6b4e6fe1e83
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2014-10-07 10:55:58 +0300

    Stub Devise authentication; refactor sponsorship_controller_spec

--- spec/controllers/admin/sponsorships_controller_spec.rb
+++ spec/controllers/admin/sponsorships_controller_spec.rb
@@ -4,50 +4,44 @@ include Devise::TestHelpers
 describe Admin::SponsorshipsController, type: :controller do
 
   let(:orphan) { instance_double Orphan }
-  let(:sponsor) { instance_double Sponsor, id: 1 }
+  let(:sponsor) { instance_double Sponsor }
+  let(:sponsorship) { instance_double Sponsorship, sponsor: sponsor, orphan: orphan }
 
   before(:each) do
-    @admin_user = AdminUser.create(email: 'admin@example.com',
-                                   password: 'password',
-                                   password_confirmation: 'password')
-    sign_in @admin_user
+    sign_in instance_double(AdminUser)
 
     allow(Sponsor).to receive(:find).with('1').and_return(sponsor)
     allow(Orphan).to receive(:find).with('1').and_return(orphan)
+    allow(Sponsorship).to receive(:find).with('1').and_return(sponsorship)
   end
 
   describe 'inactivate' do
 
-    let(:sponsorship) { instance_double Sponsorship, id: 1, sponsor: sponsor, orphan: orphan }
-
     before(:each) do
-      allow(Sponsorship).to receive(:find).with('1').and_return(sponsorship)
       allow(sponsorship).to receive(:inactivate)
+      put :inactivate, { id: 1, sponsor_id: 1 }
     end
 
     it 'assigns instance variables' do
-      put :inactivate, { id: 1, sponsor_id: sponsor.id }
       expect(assigns :sponsor).to eq sponsor
       expect(assigns :sponsorship).to eq sponsorship
     end
 
     it 'calls #inactivate on sponsorship' do
-      put :inactivate, { id: sponsorship.id, sponsor_id: sponsor.id }
       expect(sponsorship).to have_received :inactivate
     end
 
     it 'sets flash[:success] message' do
-      put :inactivate, { id: sponsorship.id, sponsor_id: sponsor.id }
       expect(flash[:success]).to eq 'Sponsorship link was successfully terminated'
     end
 
     it 'redirects to sponsor show view' do
-      put :inactivate, { id: sponsorship.id, sponsor_id: sponsor.id }
       expect(response).to redirect_to admin_sponsor_path(sponsor)
     end
   end
 
   describe 'create' do
+
     before(:each) do
       allow(Sponsorship).to receive(:create!)
       post :create, { orphan_id: 1, sponsor_id: 1 }
