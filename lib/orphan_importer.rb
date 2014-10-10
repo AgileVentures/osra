cbe76119136e103fbb698ac3625eda421cb2fff6
Motasem Salem <motasem@gmail.com>
2014-10-10 10:07:17 -0400

    Opening Excel files properly

--- lib/orphan_importer.rb
+++ lib/orphan_importer.rb
@@ -1,2 +1,29 @@
 class OrphanImporter
+
+  attr_reader :pending_orphans, :import_errors
+
+  def initialize(file)
+    @pending_orphans = []
+    @import_errors = []
+    @file = file
+    @doc = doc
+  end
+
+  def valid?
+    @import_errors.empty?
+  end
+
+  private
+
+  def doc
+    @file =~ /[.]([^.]+)\z/
+    Roo::Spreadsheet.open @file, extension: $1.to_s
+  rescue => e
+    add_validation_error('Import file','Is not a valid Excel file. ' + e.to_s)
+  end
+
+  def add_validation_error(ref, error)
+    @import_errors << {ref: ref, error: error}
+  end
+
 end
