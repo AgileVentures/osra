29c5b9c7ccc4008b1bbe3f31e45b91caed695b5c
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2014-10-15 20:05:27 +0300

    Controller specs for orphan_list#validate

--- spec/controllers/admin/orphan_lists_controller_spec.rb
+++ spec/controllers/admin/orphan_lists_controller_spec.rb
@@ -15,7 +15,7 @@ describe Admin::OrphanListsController, type: :controller do
   describe 'upload' do
     it 'redirects to partner if partner not active' do
       expect(partner).to receive(:active?).and_return false
-      get :upload, partner_id: 1, pending_orphan_list: [spreadsheet: 'sheet']
+      get :upload, partner_id: 1
       expect(response).to redirect_to admin_partner_path(1)
     end
 
@@ -32,7 +32,40 @@ describe Admin::OrphanListsController, type: :controller do
     end
   end
 
-  describe 'validate'
+  describe 'validate' do
+    let(:orphan_list_params) { { :spreadsheet => fixture_file_upload('one_orphan_xls.xls') } }
+    let(:orphan_lists) { double }
+
+    before do
+      allow(partner).to receive(:active?).and_return true
+      allow(pending_orphan_list).to receive :save!
+      allow(partner).to receive(:orphan_lists).and_return orphan_lists
+      allow(orphan_lists).to receive :build
+    end
+
+    it 'redirects to partner if partner not active' do
+      expect(partner).to receive(:active?).and_return false
+      post :validate, partner_id: 1
+      expect(response).to redirect_to admin_partner_path(1)
+    end
+
+    it 'sets instance variables' do
+      expect(pending_orphan_list).to receive(:save!)
+      post :validate, partner_id: 1, pending_orphan_list: orphan_list_params
+      expect(assigns :partner).to eq partner
+      expect(assigns :pending_orphan_list).to eq pending_orphan_list
+    end
+
+    it 'saves pending_orphan_list' do
+      expect(pending_orphan_list).to receive :save!
+      post :validate, partner_id: 1, pending_orphan_list: orphan_list_params
+    end
+
+    it 'renders :validate' do
+      post :validate, partner_id: 1, pending_orphan_list: orphan_list_params
+      expect(response).to render_template :validate
+    end
+  end
 
   describe 'import'
 end
