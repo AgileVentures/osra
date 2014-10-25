9b476910b901085b4d40f7e306324e15f02c9be0
b264 <public88878878887@hotmail.com>
2014-10-25 11:57:54 -0500

    tests for attribute sorting

--- app/admin/sponsorship.rb
+++ app/admin/sponsorship.rb
@@ -12,7 +12,6 @@ ActiveAdmin.register Sponsorship do
     panel 'Orphans' do
 
       table_for Orphan.active.currently_unsponsored.sort_by_param(params), sortable: true do
-		  column :id, sortable: :id
 		  column :osra_num, sortable: :osra_num
 		  column 'Name', sortable: :name do |_orphan|
 			link_to _orphan.name, admin_orphan_path(_orphan)
