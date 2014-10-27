dd87367d8b67e7d614b9f98e31e6fc384d4ea449
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2014-10-27 14:39:55 +0200

    Create orphan_sponsorship_status fixture & refactor orphan_sponsorship_status_spec

--- spec/models/orphan_sponsorship_status_spec.rb
+++ spec/models/orphan_sponsorship_status_spec.rb
@@ -2,8 +2,14 @@ require 'rails_helper'
 
 RSpec.describe OrphanSponsorshipStatus, :type => :model do
 
-  it 'should have a valid factory' do
-    expect(build_stubbed :orphan_sponsorship_status).to be_valid
+  # it 'should have a valid factory' do
+  #   expect(build_stubbed :orphan_sponsorship_status).to be_valid
+  # end
+
+  it 'should have valid fixtures' do
+    OrphanSponsorshipStatus.all.each do |status|
+      expect(status).to be_valid
+    end
   end
 
   it { is_expected.to have_many :orphans }
