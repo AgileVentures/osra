973167cad72b9c061968f75e7bc209015a8f82c8
motasem-salem <motasem@gmail.com>
2014-10-30 18:37:16 -0400

    Removed all hardcoded gender strings and put them in yaml config file

--- app/models/orphan.rb
+++ app/models/orphan.rb
@@ -18,7 +18,7 @@ class Orphan < ActiveRecord::Base
   validates :mother_name, presence: true
   validates :mother_alive, inclusion: {in: [true, false] }, exclusion: { in: [nil]}
   validates :date_of_birth, presence: true, date_not_in_future: true
-  validates :gender, presence: true, inclusion: {in: %w(Male Female) } # TODO: DRY list of allowed values
+  validates :gender, presence: true, inclusion: {in: Settings.lookup.gender }
   validates :contact_number, presence: true
   validates :sponsored_by_another_org, inclusion: {in: [true, false] }, exclusion: { in: [nil]}
   validates :minor_siblings_count, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
