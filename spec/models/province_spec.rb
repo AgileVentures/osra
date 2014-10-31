3950f0a4b6d13daf66aa562d3785562f07e738f6
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2014-11-01 00:01:01 +0200

    Refactor addresses & partners factories to use provinces fixture

--- spec/models/province_spec.rb
+++ spec/models/province_spec.rb
@@ -12,10 +12,6 @@ describe Province, type: :model do
     end
   end
 
-  # it 'should be able to call the factory many times' do
-  #   expect { 25.times { build_stubbed :province } }.not_to raise_error
-  # end
-
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_uniqueness_of :name }
   it { is_expected.to validate_presence_of :code }
