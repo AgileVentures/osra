efde1958c8d296d01659d85772037818a102a011
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2014-10-31 23:55:16 +0200

    Refactor province_spec

--- spec/models/province_spec.rb
+++ spec/models/province_spec.rb
@@ -2,17 +2,30 @@ require 'rails_helper'
 
 describe Province, type: :model do
 
-  it 'should have a valid factory' do
-    expect(build_stubbed :province).to be_valid
+  # it 'should have a valid factory' do
+  #   expect(build_stubbed :province).to be_valid
+  # end
+
+  it 'should have valid fixtures' do
+    Province.all.each do |province|
+      expect(province).to be_valid
+    end
   end
 
-  it 'should be able to call the factory many times' do
-    expect { 25.times { build_stubbed :province } }.not_to raise_error
-  end
+  # it 'should be able to call the factory many times' do
+  #   expect { 25.times { build_stubbed :province } }.not_to raise_error
+  # end
 
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_uniqueness_of :name }
   it { is_expected.to validate_presence_of :code }
   it { is_expected.to validate_uniqueness_of :code }
-  it { is_expected.to validate_inclusion_of(:code).in_array Province::PROVINCE_CODES }
+
+  describe 'code value validation' do
+    # to prevent codes being tested from running into uniqueness
+    # constraints with persisted provinces from fixture
+    before { Province.destroy_all }
+
+    it { is_expected.to validate_inclusion_of(:code).in_array Province::PROVINCE_CODES }
+  end
 end
