907c6d385911f142f9c64e1c581ff395ac42465f
b264 <public88878878887@hotmail.com>
2014-11-02 13:09:02 -0600

    blank payment_plan to model validation

--- app/admin/sponsor.rb
+++ app/admin/sponsor.rb
@@ -81,7 +81,7 @@ ActiveAdmin.register Sponsor do
       unless f.object.new_record?
         f.input :request_fulfilled, :input_html => { :disabled => true }
       end
-      f.input :payment_plan, as: :select, collection: Sponsor::PAYMENT_PLANS, include_blank: false
+      f.input :payment_plan, as: :select, collection: Sponsor::PAYMENT_PLANS, include_blank: true
       f.input :sponsor_type, include_blank: false
       f.input :organization
       f.input :branch
--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,6 +1,6 @@
 class Sponsor < ActiveRecord::Base
 
-  PAYMENT_PLANS = ['', 'Monthly', 'Every Two Months', 'Every Four Months', 'Every Six Months', 'Annually', 'Other']
+  PAYMENT_PLANS = ['Monthly', 'Every Two Months', 'Every Four Months', 'Every Six Months', 'Annually', 'Other']
 
   include Initializer
 
@@ -17,7 +17,7 @@ class Sponsor < ActiveRecord::Base
   validates :request_fulfilled, inclusion: {in: [true, false] }
   validates :sponsor_type, presence: true
   validates :gender, inclusion: {in: %w(Male Female) } # TODO: DRY list of allowed values
-  validates :payment_plan, allow_nil: false, inclusion: { in: PAYMENT_PLANS }
+  validates :payment_plan, allow_nil: false, allow_blank: true, inclusion: { in: PAYMENT_PLANS }
   validate :ensure_valid_date
   validate :date_not_beyond_first_of_next_month
   validate :belongs_to_one_branch_or_organization
