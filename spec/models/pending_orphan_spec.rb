eda1bcd46da12d2d214c4922af6529bb59da05f1
Craig Ferry <PurityControl@users.noreply.github.com>
2014-11-03 22:19:48 +0000

    tidied attributes hash

--- spec/models/pending_orphan_spec.rb
+++ spec/models/pending_orphan_spec.rb
@@ -2,7 +2,22 @@ require 'rails_helper'
 
 describe PendingOrphan, type: :model do
   let(:pending_orphan) {PendingOrphan.new}
-  let(:attributes) { {id: nil, pending_orphan_list_id: nil, name: "الطفل", father_name: "الأب", father_is_martyr: true, father_occupation: "مهنة الأب", father_place_of_death: "مكان الوفاة", father_cause_of_death: "سبب الوفاة", father_date_of_death: "2011-03-15", mother_name: "الأم", mother_alive: true, date_of_birth: "2009-03-15", gender: "Male", health_status: "معافى", schooling_status: "الصف الرابع", goes_to_school: true, guardian_name: "الوصي", guardian_relationship: 0, guardian_id_num: 448273, original_address_province: 12, original_address_city: "مدينة حلب", original_address_neighborhood: "الحي", original_address_street: "الشارع", original_address_details: nil, current_address_province: 13, current_address_city: "مدينة حمص", current_address_neighborhood: "حي", current_address_street: nil, current_address_details: "تفصيل", contact_number: 998716.0, alt_contact_number: "رقم آخر", sponsored_by_another_org: false, another_org_sponsorship_details: nil, minor_siblings_count: 7, sponsored_minor_siblings_count: 0, comments: "ملاحظات"} }
+  let(:attributes) { {id: nil, pending_orphan_list_id: nil, name: "الطفل",
+    father_name: "الأب", father_is_martyr: true, father_occupation: "مهنة الأب",
+    father_place_of_death: "مكان الوفاة", father_cause_of_death: "سبب الوفاة",
+    father_date_of_death: "2011-03-15", mother_name: "الأم", mother_alive: true,
+    date_of_birth: "2009-03-15", gender: "Male", health_status: "معافى",
+    schooling_status: "الصف الرابع", goes_to_school: true,
+    guardian_name: "الوصي", guardian_relationship: 0, guardian_id_num: 448273,
+    original_address_province: 12, original_address_city: "مدينة حلب",
+    original_address_neighborhood: "الحي", original_address_street: "الشارع",
+    original_address_details: nil, current_address_province: 13,
+    current_address_city: "مدينة حمص", current_address_neighborhood: "حي",
+    current_address_street: nil, current_address_details: "تفصيل",
+    contact_number: 998716.0, alt_contact_number: "رقم آخر",
+    sponsored_by_another_org: false, another_org_sponsorship_details: nil,
+    minor_siblings_count: 7, sponsored_minor_siblings_count: 0,
+    comments: "ملاحظات"} }
 
   specify '#create_orphan' do
     orphan = pending_orphan.orphan
