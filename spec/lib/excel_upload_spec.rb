5977b87058c95ca6cd889cd6253ba8bbe6232cff
Craig Ferry <PurityControl@users.noreply.github.com>
2014-11-03 08:07:37 +0000

    rafactor orphan importer and tests
    OSRA-238
    split concerns of orphan imports among
    - orphan importer
    - excel upload
    - import orphan settings
    - pending orphans

---
+++ spec/lib/excel_upload_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+require 'excel_upload'
+
+describe ExcelUpload do
+  before :each do
+    @settings = Settings.import
+  end
+
+  describe '.upload' do
+    it 'should reject opening a non Excel file with an error' do
+      expect{ExcelUpload.upload('spec/fixtures/not_an_excel_file.txt',
+        @settings.first_row)}.to raise_error
+    end
+
+    it 'should open an .xls file with no errors' do
+      expect{ExcelUpload.upload('spec/fixtures/one_orphan_xls.xls',
+        @settings.first_row)}.not_to raise_error
+    end
+
+    it 'should reject opening an empty Excel file' do
+      expect{ExcelUpload.upload('spec/fixtures/empty_xls.xls',
+        @settings.first_row)}.to raise_error
+    end
+
+    it 'should reject opening a non Excel file even if it has an Excel extension' do
+      expect{ExcelUpload.upload('spec/fixtures/fake_excel_file.png.xls',
+        @settings.first_row)}.to raise_error
+    end
+  end
+
+  describe '.extension' do
+    it 'should return .xls for a file ending in .xls' do
+      expect(ExcelUpload.extension('file.xls')).to eq 'xls'
+    end
+
+    it 'should return the last extension for files with multiples extensions' do
+      expect(ExcelUpload.extension('file.png.txt')).to eq 'txt'
+    end
+  end
+
+  describe '.empty_file?' do
+    it 'should return true for an file without orphan records' do
+      doc = Roo::Spreadsheet.open('spec/fixtures/empty_xls.xls')
+      expect(ExcelUpload.empty_file?(doc, @settings.first_row)).to be true
+    end
+
+    it 'should return true for an file without orphan records' do
+      doc = Roo::Spreadsheet.open('spec/fixtures/one_orphan_xls.xls')
+      expect(ExcelUpload.empty_file?(doc, @settings.first_row)).to be false
+    end
+  end
+end
---
+++ spec/models/pending_orphan_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+describe PendingOrphan, type: :model do
+  let(:pending_orphan) {PendingOrphan.new}
+  let(:attributes) { {id: nil, pending_orphan_list_id: nil, name: "الطفل", father_name: "الأب", father_is_martyr: true, father_occupation: "مهنة الأب", father_place_of_death: "مكان الوفاة", father_cause_of_death: "سبب الوفاة", father_date_of_death: "2011-03-15", mother_name: "الأم", mother_alive: true, date_of_birth: "2009-03-15", gender: "Male", health_status: "معافى", schooling_status: "الصف الرابع", goes_to_school: true, guardian_name: "الوصي", guardian_relationship: 0, guardian_id_num: 448273, original_address_province: 12, original_address_city: "مدينة حلب", original_address_neighborhood: "الحي", original_address_street: "الشارع", original_address_details: nil, current_address_province: 13, current_address_city: "مدينة حمص", current_address_neighborhood: "حي", current_address_street: nil, current_address_details: "تفصيل", contact_number: 998716.0, alt_contact_number: "رقم آخر", sponsored_by_another_org: false, another_org_sponsorship_details: nil, minor_siblings_count: 7, sponsored_minor_siblings_count: 0, comments: "ملاحظات"} }
+
+  specify '.create_orphan' do
+    orphan = pending_orphan.instance_variable_get(:@orphan)
+    expect(orphan.class.name).to eq 'Orphan'
+  end
+
+  specify '.to_orphan' do
+    pending_orphan.attributes = attributes
+    orphan = pending_orphan.to_orphan
+    expect(orphan.class.name).to eq 'Orphan'
+  end
+
+  specify '.address_prefix' do
+    expect(pending_orphan.address_prefix('address')).to eq 'address_'
+  end
+
+  specify '.pending_attrs_to_orphan' do
+    remove_hash = {:pending => 'val', :address => 'val', :id => 'val'}
+    expect(pending_orphan).to receive(:attributes).and_return(remove_hash)
+    expect(pending_orphan.pending_attrs_to_orphan).to eq Hash.new
+  end
+
+  specify '.extract_and_create_address' do
+    expect(pending_orphan).to receive(:attributes).and_return(attributes)
+    expect(Province).to receive(:find_by_code).and_return(create :province)
+    pending_orphan.extract_and_create_address('original_address')
+    expect(pending_orphan.instance_variable_get(:@orphan).original_address).to be_valid
+  end
+
+  specify '.fields_starting_with' do
+    start_hash = {key1: 'val', key2: 'val', other_key: 'val'}
+    end_hash = {key1: 'val', key2: 'val'}
+    expect(pending_orphan.fields_starting_with(start_hash, 'key')).
+      to eq end_hash
+  end
+
+  specify '.reject_fields_starting_with' do
+    pending_orphan.reject_fields_starting_with(attributes, 'original_address')
+    expect(pending_orphan.attributes[:current_address_city]).to be nil
+  end
+
+  specify '.remove_prefix_from_keys' do
+    start_hash = {original_address_street: 'val', 
+      original_address_city: 'val'}
+    end_hash = {"street" => 'val', "city" => 'val'}
+    expect(pending_orphan.remove_prefix_from_keys(start_hash,
+      'original_address_')).to eq end_hash
+  end
+end
