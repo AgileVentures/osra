970885c1e917413ea14347f0425c7f9289440eb5
b264 <public88878878887@hotmail.com>
2014-11-04 08:04:56 -0600

    update new paths

--- features/step_definitions/admin/sponsors_steps.rb
+++ features/step_definitions/admin/sponsors_steps.rb
@@ -24,10 +24,18 @@ end
 
 When(/^I (?:go to|am on) the "([^"]*)" page for sponsor "([^"]*)"$/) do |page, sponsor_name|
   sponsor = Sponsor.find_by name: sponsor_name
-  visit path_to_admin_role(page, sponsor.id)
+  unless page== 'Link to Orphan'
+    visit path_to_admin_role(page, sponsor.id)
+  else
+    visit new_sponsorship_path(sponsor.id)
+  end
 end
 
 Then(/^I should be on the "(.*?)" page for sponsor "(.*?)"$/) do |page_name, sponsor_name|
   sponsor = Sponsor.find_by name: sponsor_name
-  expect(current_path).to eq path_to_admin_role(page_name, sponsor.id)
+  unless page_name== 'Link to Orphan'
+    expect(current_path).to eq path_to_admin_role(page_name, sponsor.id)
+  else
+    expect(/\/admin\/sponsors\/(\d+)\/sponsorships\/new/ =~ current_path).to be > 0
+  end
 end
