675552e5015c719e39e0467f586859aa7c4e70b9
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2014-11-07 17:39:52 +0200

    Code review: add unique indexes on user_name & email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110225315) do
+ActiveRecord::Schema.define(version: 20141110225316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -274,5 +274,8 @@ ActiveRecord::Schema.define(version: 20141110225315) do
     t.datetime "updated_at"
     t.string   "user_name"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["user_name"], name: "index_users_on_user_name", unique: true, using: :btree
 
 end
