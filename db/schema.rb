0996bec4bcd4e71d3b4e9dd0466c87d47201ddc1
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2014-11-07 12:20:23 +0200

    Change migration timestamp to resolve conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110225312) do
+ActiveRecord::Schema.define(version: 201411102253121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
