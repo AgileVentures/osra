46eed5a4f50c9a29ff419e2a25429f4d0ee0c6d9
b264 <public88878878887@hotmail.com>
2014-11-10 14:53:08 -0600

    remove unneccessary view

--- app/admin/sponsorship.rb
+++ app/admin/sponsorship.rb
@@ -3,32 +3,6 @@ ActiveAdmin.register Sponsorship do
   actions :create
   belongs_to :sponsor
 
-  form do
-    panel 'Sponsor' do
-      h3 sponsorship.sponsor_name
-      para link_to 'Return to Sponsor page', admin_sponsor_path(sponsorship.sponsor)
-      para sponsorship.sponsor_additional_info
-    end
-
-    panel 'Orphans' do
-      table_for Orphan.active.currently_unsponsored do
-        column :id
-        column 'Name' do |_orphan|
-          link_to _orphan.name, admin_orphan_path(_orphan)
-        end
-        column :gender
-        column :father_alive
-        column :mother_alive
-        column 'Establish sponsorship' do |_orphan|
-          link_to 'Sponsor this orphan',
-                  admin_sponsor_sponsorships_path(sponsor_id: sponsorship.sponsor_id,
-                                                orphan_id: _orphan.id),
-                  method: :post
-        end
-      end
-    end
-  end
-
   member_action :inactivate, method: :put do
     @sponsorship = Sponsorship.find(params[:id])
     @sponsor = Sponsor.find(params[:sponsor_id])
