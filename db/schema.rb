5bde386ef4d46e85a126dda4b0873cf5ca70b69b
motasem-salem <motasem@gmail.com>
2014-11-11 11:34:36 -0500

    Added validations for father_alive, father_is_martyr and father_date_of_death values

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110225318) do
+ActiveRecord::Schema.define(version: 20141110225319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -241,8 +241,8 @@ ActiveRecord::Schema.define(version: 20141110225318) do
     t.integer  "requested_orphan_count"
     t.boolean  "request_fulfilled",      default: false, null: false
     t.integer  "agent_id"
-    t.string   "payment_plan",           default: "",    null: false
     t.string   "city"
+    t.string   "payment_plan",           default: "",    null: false
   end
 
   add_index "sponsors", ["agent_id"], name: "index_sponsors_on_agent_id", using: :btree
