16364f5ba6e9f8d0526ef5bd59a5490187ce6eb7
b264 <public88878878887@hotmail.com>
2014-11-12 01:38:21 -0600

    add failing tests for filtering; remove custom route (bugfix)

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,4 @@ Osra::Application.routes.draw do
   devise_for :users
 
   root to: "admin/dashboard#index"
-
-  #build a new sponsorship on the Orphan class instead of the Sponsorship class
-  get '/admin/sponsors/:sponsor_id/sponsorships/new', to: 'admin/orphans#index', as: :new_sponsorship
-
 end
