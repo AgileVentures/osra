49d06b13c67a9ab16bf89e5fcac1c0321f10c11a
Robbie Jackson <robbie.jackson@btinternet.com>
2014-11-28 20:54:58 +0000

    intermediate commit for organizations fixtures

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-
+=begin
 describe Organization, type: :model do
 
   it 'has a valid factory' do
@@ -17,4 +17,18 @@ describe Organization, type: :model do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_uniqueness_of :name }
   end
+end
+=end
+describe Organization, type: :model do
+
+  it 'should have valid fixtures' do
+    Organization.all.each do |org|
+      expect(org).to be_valid
+    end
+  end
+
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_uniqueness_of :name }
+  it { is_expected.to validate_presence_of :code }
+  it { is_expected.to validate_uniqueness_of :code }
 end
