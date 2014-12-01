df0a5e9bb037306b37728b4d6627c86931e16d15
b264 <public88878878887@hotmail.com>
2014-11-30 20:02:25 -0600

    code review: const factor

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,11 +1,13 @@
 ActiveAdmin.register_page "Dashboard" do
 
+  IMAGE_SCALING = 0.38
+
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content do
     div class: "blank_slate_container" do
-      height= (865 * 0.4).to_i.to_s
-      width= (960 * 0.4).to_i.to_s
+      height= (865 * IMAGE_SCALING).to_i.to_s
+      width= (960 * IMAGE_SCALING).to_i.to_s
       image= '<img src="/osra_logo.jpg" height="' + height + '" width="' + width + '" alt="Welcome to OSRA" />'
       text_node image.html_safe
     end
