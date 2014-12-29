f7d9ff716e896a9b8da0cc58be0cd1d8a9340514
b264 <public88878878887@hotmail.com>
2014-12-29 02:47:05 -0600

    double negative spec

--- spec/views/hq/partners/index_spec.rb
+++ spec/views/hq/partners/index_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
 describe "hq/partners/index.html.erb", type: :view do
-  let(:partners) do
-    3.times do
-      FactoryGirl.create :partner
-    end
-    Partner.all
-  end
-
   context 'partners exist' do
+    let(:partners) do
+      3.times do
+        FactoryGirl.create :partner
+      end
+      Partner.all
+    end
+
     before :each do
       assign(:partners, partners)
     end
 
-
-
-
+    it 'should not indicate no partners were found' do
+      render and expect(rendered).to_not match /No Partners found/
+    end
   end
 
   context 'no partners exist' do
