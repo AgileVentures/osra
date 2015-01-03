cd3fe303d01fd22ee43e166f3915bda1d01069db
Cristian <razorcd@yahoo.com>
2015-01-03 14:48:45 +0200

    filtering flash messages to show only the accepted ones

--- app/helpers/_flashes_helper.rb
+++ app/helpers/_flashes_helper.rb
@@ -1,12 +1,22 @@
 module FlashesHelper
-  
+
+  ACCEPTED_FLASH_HTML_CLASSES = {
+    success: 'alert alert-success',
+    error: 'alert alert-danger'
+  }  
+
   def flash_htmlclass_for type
-    case type.to_sym 
-      when :success 
-        'alert alert-success'
-      when :error
-        'alert alert-danger'
+    ACCEPTED_FLASH_HTML_CLASSES[type.to_sym] || nil
+  end
+
+  def each_accepted_flash
+    flash.each do |fl|
+      yield fl if flash_htmlclass_for fl[0]
     end
+  end
+
+  def any_accepted_flash?
+    flash.any? {|k,_| flash_htmlclass_for k }
   end
 
 end
--- spec/views/layouts/_flashes_spec.rb
+++ spec/views/layouts/_flashes_spec.rb
@@ -4,13 +4,21 @@ RSpec.describe 'layouts/_flashes.html.erb', type: :view do
   context 'flash messages defined' do
     let(:flash_success) {flash[:success] = 'Success message!'}
     let(:flash_error) {flash[:error] = 'Error message!'}
+    let(:flash_other) {flash[:other] = 'Other message!'}
     
-    it 'should show flash messages' do
+    it 'should show accepted flash messages' do
       flash_success and flash_error
       render
       expect(rendered).to have_selector '.flashes'
       expect(rendered).to match /Success message!/
       expect(rendered).to match /Error message!/
+    end
+
+    it 'should not show unaccepted flash messages' do
+      flash_other
+      render
+      expect(rendered).to_not have_selector '.flashes'
+      expect(rendered).to_not match /Other message!/
     end
   end
 
