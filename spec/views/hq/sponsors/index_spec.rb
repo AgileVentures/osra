1badad501c3e92ceac5dce6a18c96fcc35e6e188
b264 <public88878878887@hotmail.com>
2015-01-11 16:03:14 -0600

    #index view spec update

--- spec/views/hq/sponsors/index_spec.rb
+++ spec/views/hq/sponsors/index_spec.rb
@@ -1,14 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "hq/sponsors/index.html.haml", type: :view do
-  context 'sponsors exist' do
+  let(:sponsors) do
+    [ FactoryGirl.build_stubbed(:sponsor), FactoryGirl.build_stubbed(:sponsor)]
+  end
+
+  describe 'sponsors exist' do
+    before :each do
+      assign(:sponsors, sponsors)
+    end
+
     it 'should not indicate no sponsors were found' do
-      assign(:sponsors, [ FactoryGirl.build_stubbed(:sponsor) ] )
       render and expect(rendered).to_not match /No Sponsors found/
+    end
+
+    it 'should link to #show actions' do
+      render
+      sponsors.each do |sponsor|
+        expect(rendered).to have_link(sponsor.name, hq_sponsor_path(sponsor.id))
+      end
     end
   end
 
-  context 'no sponsors exist' do
+  describe 'no sponsors exist' do
     it 'should indicate no sponsors were found' do
       assign(:sponsors, [])
       render and expect(rendered).to match /No Sponsors found/
