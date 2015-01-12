ab565cf236d78e7df990f60b3f114b5b7d683c83
Robbie Jackson <robbie.jackson@btinternet.com>
2015-01-12 22:46:09 +0000

    Rails edit and update for Users implemented

---
+++ spec/views/hq/users/_form.html.erb_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe "hq/users/_form.html.erb", type: :view do
+
+  before :each do
+    @user = FactoryGirl.build_stubbed(:user)
+    render
+  end
+
+  it 'has a form element' do
+    expect(rendered).to have_selector('form')
+  end
+
+  it 'has an input for user name, prefilled with current value' do
+    expect(rendered).to have_selector("input\#user_user_name[value=\"#{@user.user_name}\"]")
+  end
+
+  it 'has an input for email address, prefilled with current value' do
+    expect(rendered).to have_selector("input\#user_email[value=\"#{@user.email}\"]")
+  end
+
+  it 'has a submit button' do
+    expect(rendered).to have_selector('input[type="submit"]')
+  end
+
+  it 'has a cancel button' do
+    expect(rendered).to have_link('Cancel', href: hq_user_path(@user))
+  end
+
+end
