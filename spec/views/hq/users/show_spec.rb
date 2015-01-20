60170a1b0cfa345d223994852e34a671288b43c9
b264 <public88878878887@hotmail.com>
2015-01-20 00:15:27 -0600

    sponsor index partial spec

--- spec/views/hq/users/show_spec.rb
+++ spec/views/hq/users/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'hq/users/show.html.erb', type: :view do
-  describe 'users' do
+  describe 'user' do
     let(:user) { FactoryGirl.build_stubbed :user }
 
     before :each do
@@ -20,60 +20,36 @@ RSpec.describe 'hq/users/show.html.erb', type: :view do
     end
   end
 
-  [['active', 'Active'], ['active', 'On Hold'], ['inactive', 'Inactive']].each do |status|
-    describe "#{status[0]} (#{status[1]}) sponsors" do
-      describe "don't exist" do
-        let!(:user) { FactoryGirl.build_stubbed :user }
+  specify 'with no active nor inactive sponsors' do
+    assign :user, FactoryGirl.build_stubbed(:user)
+    render
 
-        before :each do
-          assign :user, user
-          render
-        end
-
-        specify "don't display" do
-          assert_select 'div[class=?]', "#{status[0]}_sponsors_index", false
-        end
-
-        specify 'total number' do
-          expect(rendered).to match /0 #{status[0].titlecase} Sponsors/
-        end
-      end
-
-      eval %Q[
-        describe 'exist' do
-          let!(:user) { FactoryGirl.build_stubbed :user }
-          let(:an_#{status[0]}_status) { Status.find_by_name '#{status[1]}' }
-          let!(:#{status[0]}_sponsor) { FactoryGirl.create(:sponsor, status: an_#{status[0]}_status, agent: user) }
-
-          before :each do
-            assign :user, user
-            render
-          end
-
-          describe 'display' do
-            specify 'total number' do
-              expect(rendered).to match /1 #{status[0].titlecase} Sponsor/
-            end
-
-            ['osra_num', 'name', 'status.name', 'sponsor_type.name'].each do |attrib|
-              eval %Q[
-                specify attrib do
-                  assert_select 'div[class=?]', "#{status[0]}_sponsors_index" do |elements|
-                    assert_select 'td', text: #{status[0]}_sponsor.\#{attrib}
-                  end
-                end
-              ]
-            end
-
-            specify "link to #{status[0]} sponsor" do
-              expect(rendered).to have_link(#{status[0]}_sponsor.osra_num,
-                                            hq_sponsor_path(#{status[0]}_sponsor))
-            end
-          end
-        end
-      ]
-    end
+    expect(view).to_not render_template partial: 'hq/sponsors/index.html.haml',
+                                      locals: {sponsors: []}
   end
 
+  describe 'with sponsors' do
+    let!(:user) { FactoryGirl.build_stubbed :user }
+    let!(:sponsors) do
+      [ FactoryGirl.create(:sponsor, agent: user, status: Status.find_by_name('Active')),
+        FactoryGirl.create(:sponsor, agent: user, status: Status.find_by_name('On Hold')),
+        FactoryGirl.create(:sponsor, agent: user, status: Status.find_by_name('Inactive'))
+      ]
+    end
 
+    before :each do
+      assign :user, user
+      render
+    end
+
+    specify 'inactive' do
+      expect(view).to render_template partial: 'hq/sponsors/index.html.haml',
+                                      locals: {sponsors: user.sponsors.all_active}
+    end
+
+    specify 'active' do
+      expect(view).to render_template partial: 'hq/sponsors/index.html.haml',
+                                      locals: {sponsors: user.sponsors.all_inactive}
+    end
+  end
 end
