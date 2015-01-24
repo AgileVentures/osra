5eed5f7acaf2a142704d0e96ac64881a0d78cf13
Cristian <razorcd@yahoo.com>
2015-01-24 18:59:49 +0200

    rebactored form partial based on code reviews

--- spec/views/hq/sponsors/_form_spec.rb
+++ spec/views/hq/sponsors/_form_spec.rb
@@ -3,7 +3,7 @@ require 'cgi'
 
 RSpec.describe "hq/sponsors/_form.html.haml", type: :view do
   let(:user) { build_stubbed :user}
-  let(:sponsor_full) do 
+  let(:sponsor_full) do
     build_stubbed :sponsor_full, agent: user,
                    request_fulfilled: [true, false].sample
   end
@@ -46,11 +46,11 @@ RSpec.describe "hq/sponsors/_form.html.haml", type: :view do
       render
 
       #fextfields
-      ["name", "requested_orphan_count", "start_date", "new_city_name", "address", "email", 
+      ["name", "requested_orphan_count", "start_date", "new_city_name", "address", "email",
        "contact1", "contact2", "additional_info"].each do |field|
         assert_select "input#sponsor_#{field}" do
           if sponsor_full[field]
-            assert_select "[value=?]", CGI::escape_html(sponsor_full[field].to_s) 
+            assert_select "[value=?]", CGI::escape_html(sponsor_full[field].to_s)
           else
             assert_select "[value]", false
           end
@@ -69,9 +69,8 @@ RSpec.describe "hq/sponsors/_form.html.haml", type: :view do
 
       assert_select "input#sponsor_request_fulfilled" do
         assert_select "[disabled=?]", "disabled"
-        assert_select "[value=?]", sponsor_full.request_fulfilled ? "1" : "0"
-        assert_select "[checked]", true if sponsor_full.request_fulfilled
-        assert_select "[checked]", false if !sponsor_full.request_fulfilled
+          assert_select "[checked]", sponsor_full.request_fulfilled
+        end
       end
 
       assert_select "select#sponsor_sponsor_type_id" do
@@ -85,21 +84,27 @@ RSpec.describe "hq/sponsors/_form.html.haml", type: :view do
       assert_select "select#sponsor_organization_id" do
         assert_select "[disabled=?]", "disabled"
         assert_select "option", value: sponsor_full.organization_id
-        assert_select "option", 
-            html: CGI::escape_html(sponsor_full.organization.name) if sponsor_full.organization
-        assert_select "option", value: false if !sponsor_full.organization
+        if sponsor_full.organization
+          assert_select "option",
+            html: CGI::escape_html(sponsor_full.organization.name)
+        else
+          assert_select "option", value: false
+        end
       end
 
       assert_select "select#sponsor_branch_id" do
         assert_select "[disabled=?]", "disabled"
         assert_select "option", value: sponsor_full.branch_id
-        assert_select "option", 
-            html: CGI::escape_html(sponsor_full.branch.name) if sponsor_full.branch
-        assert_select "option", value: false if !sponsor_full.branch
+        if sponsor_full.branch
+          assert_select "option",
+            html: CGI::escape_html(sponsor_full.branch.name)
+        else
+          assert_select "option", value: false
+        end
       end
 
       assert_select "select#sponsor_payment_plan" do
-        assert_select "option", value: sponsor_full.payment_plan,   
+        assert_select "option", value: sponsor_full.payment_plan,
                                 html: CGI::escape_html(sponsor_full.payment_plan) do
         end
       end
@@ -108,7 +113,7 @@ RSpec.describe "hq/sponsors/_form.html.haml", type: :view do
         assert_select "option", value: sponsor_full.country do
           assert_select "[selected]", html: CGI::escape_html(en_ar_country(sponsor_full.country).strip)
         end
-                                
+
       end
 
       assert_select "select#sponsor_city" do
@@ -118,9 +123,12 @@ RSpec.describe "hq/sponsors/_form.html.haml", type: :view do
 
       assert_select "select#sponsor_agent_id" do
         assert_select "option", value: sponsor_full.agent_id
-        assert_select "option", 
-            html: CGI::escape_html(sponsor_full.agent.user_name) if sponsor_full.agent
-        assert_select "option", value: false if !sponsor_full.agent
+        if sponsor_full.agent
+          assert_select "option",
+            html: CGI::escape_html(sponsor_full.agent.user_name)
+        else
+          assert_select "option", value: false
+        end
       end
 
       assert_select "input", type: "submit"
