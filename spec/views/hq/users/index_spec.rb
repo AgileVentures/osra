f45c179ba3497361ef66d69b357f44c6987ed66a
b264 <public88878878887@hotmail.com>
2015-02-01 02:43:23 -0600

    paginate users#index

--- spec/views/hq/users/index_spec.rb
+++ spec/views/hq/users/index_spec.rb
@@ -12,38 +12,34 @@ describe "hq/users/index.html.erb", type: :view do
   end
 
   describe 'should display users correctly' do
-
     context 'no users' do
-      it 'displays just the header row' do
+      it 'hides the table' do
         @users = []
         render
-        expect(rendered).to have_selector('table tr', count: 1)
+
+        expect(rendered).to match /No Users Found/
+        assert_select 'table#users-index', count: 0
       end
     end
 
     context '1 user' do
       it 'displays a single user' do
-        @users = [ double('User', user_name: 'Frederick Bloggs',
-          email: 'fred.bloggs@example.com', active_sponsors: (1..5).to_a) ]
+        @users = [ FactoryGirl.build_stubbed(:user, user_name: 'Frederick Bloggs',
+          email: 'fred.bloggs@example.com') ].paginate
         render
 
         expect(rendered).to have_selector('table tr', count: 2)
         expect(rendered).to have_link('Frederick Bloggs', href: hq_user_path(@users[0]))
         expect(rendered).to match 'fred.bloggs@example.com'
-        expect(rendered).to match '5'
       end
     end
 
     context '>1 user' do
       before :each do
-        @users = [ double('User', user_name: 'Frederick Bloggs',
-          email: 'fred.bloggs@example.com', active_sponsors: (1..5).to_a),
-                   double('User', user_name: 'Joseph Soap',
-          email: 'joe.soap@example.com', active_sponsors: (1..7).to_a) ]
-
-        (1..5).each_with_object(@users) do |num, users|
-          users << FactoryGirl.build_stubbed(:user)
-        end.paginate(:page => 2, :per_page => 3)
+        @users= [ FactoryGirl.build_stubbed(:user, user_name: 'Frederick Bloggs',
+                  email: 'fred.bloggs@example.com'),
+                  FactoryGirl.build_stubbed(:user, user_name: 'Joseph Soap',
+                  email: 'joe.soap@example.com') ].paginate(:page => 1, :per_page => 2)
       end
 
       it 'displays multiple users' do
@@ -51,10 +47,8 @@ describe "hq/users/index.html.erb", type: :view do
         expect(rendered).to have_selector('table tr', count: 3)
         expect(rendered).to have_link('Frederick Bloggs', href: hq_user_path(@users[0]))
         expect(rendered).to match 'fred.bloggs@example.com'
-        expect(rendered).to match '5'
         expect(rendered).to have_link('Joseph Soap', href: hq_user_path(@users[1]))
         expect(rendered).to match 'joe.soap@example.com'
-        expect(rendered).to match '7'
       end
 
       it 'paginates' do
