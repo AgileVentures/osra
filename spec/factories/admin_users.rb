3fd19a1076315cd9e71c8833b1958d27210b4db0
Gabriel Muñumel <gabrielmunumel@gmail.com>
2015-02-16 12:48:50 +0000

    Added sequence for email in AdminUser's factory

--- spec/factories/admin_users.rb
+++ spec/factories/admin_users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :admin_user do
-    email { [Faker::Internet.email].sample }
+    sequence(:email) { |n| "#{n}#{Faker::Internet.email}" }
     password { Faker::Internet.password }
     password_confirmation { |u| u.password }
   end
