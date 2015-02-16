ea9f46173fe6a65db5939e96dc16bf5a625e7c75
Gabriel Muñumel <gabrielmunumel@gmail.com>
2015-02-16 00:59:38 +0000

    Fixed error Email can't be nil

--- spec/factories/admin_users.rb
+++ spec/factories/admin_users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :admin_user do
-    email { [Faker::Internet.email, nil].sample }
+    email { [Faker::Internet.email].sample }
     password { Faker::Internet.password }
     password_confirmation { |u| u.password }
   end
