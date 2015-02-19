b377ee2c47859630af880cce3ce99f279dc79000
Gabriel Muñumel <gabrielmunumel@gmail.com>
2015-02-19 20:46:08 +0000

    Fixed rspec test

--- spec/routing/hq/orphans_routing_spec.rb
+++ spec/routing/hq/orphans_routing_spec.rb
@@ -3,26 +3,26 @@ require 'rails_helper'
 RSpec.describe 'Hq::Orphans routing', type: :routing do
   describe 'CRUD' do
     specify '#index' do
-      expect(get: '/hq/orphans').        not_to  be_routable
+      expect(get: '/hq/orphans').       to be_routable
     end
     specify '#create' do
-      expect(post: '/hq/orphans').       not_to  be_routable
+      expect(post: '/hq/orphans').      to be_routable
     end
     specify '#new' do
-      expect(get: '/hq/orphans/new').    not_to  be_routable
+      expect(get: '/hq/orphans/new').   to be_routable
     end
     specify '#show' do
-      expect(get: '/hq/orphans/1').      not_to  be_routable
+      expect(get: '/hq/orphans/1').     to be_routable
     end
     specify '#edit' do
-      expect(get: '/hq/orphans/1/edit'). not_to  be_routable
+      expect(get: '/hq/orphans/1/edit').to be_routable
     end
     specify '#update' do
-      expect(put: '/hq/orphans/1').      not_to  be_routable
-      expect(patch: '/hq/orphans/1').    not_to  be_routable
+      expect(put: '/hq/orphans/1').     to be_routable
+      expect(patch: '/hq/orphans/1').   to be_routable
     end
     specify '#destroy' do
-      expect(delete: '/hq/orphans/1').   not_to  be_routable
+      expect(delete: '/hq/orphans/1').  not_to be_routable
     end
   end
 end
