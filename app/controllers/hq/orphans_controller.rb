7b6d25d68a67f9934a7aa0cb9d2efa3837327578
Christopher Milne <christopher.milne@gmail.com>
2015-03-13 11:49:53 -0400

    resolve rebase merge conflicts

--- app/controllers/hq/orphans_controller.rb
+++ app/controllers/hq/orphans_controller.rb
@@ -9,5 +9,52 @@ class Hq::OrphansController < HqController
   end
 
   def create
+    @orphan ||= Orphan.new
+    @orphan.attributes = orphan_params
+    save_orphan or re_render 'new'
+  end
+
+  def edit
+    load_orphan
+  end
+
+  def update
+    load_orphan
+    build_orphan
+    save_orphan or re_render 'edit'
+  end
+
+  private
+  def load_orphan
+    @orphan = Orphan.find(params[:id])
+  end
+
+  def orphan_params
+    params.require(:orphan)
+      .permit(:contact_number, :date_of_birth, :family_name, :father_alive,
+              :father_given_name, :father_is_martyr, :gender, :minor_siblings_count,
+              :mother_alive, :mother_name, :name, :orphan_list_id, :orphan_sponsorship_status_id,
+              :orphan_status_id, :priority, :sponsored_by_another_org, :sponsored_minor_siblings_count)
+  end
+
+  def build_orphan
+    @orphan ||= Orphan.new
+    @orphan.attributes = orphan_params
+  end
+
+  def load_associations
+    #TODO what associations belong here?
+  end
+
+  def save_orphan
+    if @orphan.save
+      flash[:success] = 'Orphan successfuly saved'
+      redirect_to hq_orphan_url(@orphan)
+    end
+  end
+
+  def re_render(view)
+    load_associations
+    render view
   end
 end
