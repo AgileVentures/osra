97be75be605b3819a3e29f104a3af66999210679
Christopher Milne <christopher.milne@gmail.com>
2015-03-13 11:41:27 -0400

    resolve merge conflicts

--- app/controllers/hq/orphans_controller.rb
+++ app/controllers/hq/orphans_controller.rb
@@ -3,4 +3,11 @@ class Hq::OrphansController < HqController
   def show
     @orphan = Orphan.find(params[:id])
   end
+
+  def index
+    @orphans = Orphan.paginate(:page => params[:page])
+  end
+
+  def create
+  end
 end
