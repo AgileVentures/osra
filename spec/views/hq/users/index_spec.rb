536b4b79a64c85846426127a329c7051b71b9019
Razor <razorcd@yahoo.com>
2015-03-13 19:10:08 +0200

    rebase and small refactorings

--- spec/views/hq/users/index_spec.rb
+++ spec/views/hq/users/index_spec.rb
@@ -5,8 +5,9 @@ describe "hq/users/index.html.erb", type: :view do
 
   context 'class action-items' do
     it 'should have a New User link' do
-      assign(:users, [].paginate)
+      assign(:users, User.none.paginate(:page => 1))
       render
+
       expect(rendered).to have_link('New User', new_hq_user_path)
     end
   end
@@ -14,11 +15,11 @@ describe "hq/users/index.html.erb", type: :view do
   describe 'should display users correctly' do
     context 'no users' do
       it 'hides the table' do
-        @users = [].paginate
+        @users = User.none.paginate(:page => 1)
         render
 
         expect(rendered).to match /No Users Found/
-        assert_select 'table#users-index', count: 0
+        expect(rendered).to have_selector('table#users-index', count: 0)
       end
     end
 
@@ -36,19 +37,21 @@ describe "hq/users/index.html.erb", type: :view do
 
     context '>1 user' do
       before :each do
-        @users= [ FactoryGirl.build_stubbed(:user, user_name: 'Frederick Bloggs',
-                  email: 'fred.bloggs@example.com'),
+        @users= [ FactoryGirl.build_stubbed(:user),
                   FactoryGirl.build_stubbed(:user, user_name: 'Joseph Soap',
-                  email: 'joe.soap@example.com') ].paginate(:page => 1, :per_page => 2)
+                  email: 'joe.soap@example.com'),
+                  FactoryGirl.build_stubbed(:user, user_name: 'Tom Jones',
+                  email: 'tom.jones@example.com') ].paginate(:page => 2, :per_page => 2)
       end
 
       it 'displays multiple users' do
         render
-        expect(rendered).to have_selector('table tr', count: 3)
-        expect(rendered).to have_link('Frederick Bloggs', href: hq_user_path(@users[0]))
-        expect(rendered).to match 'fred.bloggs@example.com'
-        expect(rendered).to have_link('Joseph Soap', href: hq_user_path(@users[1]))
-        expect(rendered).to match 'joe.soap@example.com'
+
+        expect(rendered).to have_selector('table tr', count: 2)
+        expect(rendered).to have_link('Tom Jones', href: hq_user_path(@users[0]))
+        expect(rendered).to match 'tom.jones@example.com'
+        expect(rendered).to_not have_link('Joseph Soap')
+        expect(rendered).to_not match 'joe.soap@example.com'
       end
 
       it 'paginates' do
