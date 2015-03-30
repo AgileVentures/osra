58298d2519f0fb9473e6c803b869c4d37d14546f
Razor <razorcd@yahoo.com>
2015-03-30 16:10:48 +0300

    index spec only tests view related logic

--- spec/views/hq/users/index_spec.rb
+++ spec/views/hq/users/index_spec.rb
@@ -2,17 +2,23 @@ require 'rails_helper'
 
 describe "hq/users/index.html.erb", type: :view do
 
-  context 'class action-items' do
+  context 'User action-items' do
     it 'should have a New User link' do
       assign(:users, User.none.paginate(:page => 1))
       render
 
       expect(rendered).to have_link('New User', new_hq_user_path)
     end
+
+    it 'should have a pagination bar' do
+      assign(:users, build_stubbed_list(:user,1))
+      expect(view).to receive(:will_paginate)
+      render
+    end
   end
 
-  describe 'should display users correctly with pagination' do
-    context 'no users' do
+  describe 'should display users correctly' do
+    context 'for no users' do
       before(:each) do
         @users = User.none.paginate(:page => 1)
         render
@@ -22,60 +28,24 @@ describe "hq/users/index.html.erb", type: :view do
         expect(rendered).to match /No Users Found/
         expect(rendered).to have_selector('table#users-index', count: 0)
       end
-
-      it 'hides the pagination bar' do
-        expect(rendered).to_not have_selector('.pagination', text: /Previous(.*)1(.*)Next/)
-      end
     end
 
-    context '1 user' do
-      before(:each) do
-        @users = [ FactoryGirl.build_stubbed(:user, 
-                   user_name: 'Frederick Bloggs',
-                   email: 'fred.bloggs@example.com') ].paginate
-        render
-      end
-
-      it 'displays a single user' do
-        expect(rendered).to have_selector('table tbody tr', count: 1)
-        expect(rendered).to have_link('Frederick Bloggs', href: hq_user_path(@users.first))
-        expect(rendered).to match 'fred.bloggs@example.com'
-      end
-
-      it 'hides the pagination bar' do
-        expect(rendered).not_to have_selector('.pagination', text: /Previous(.*)1(.*)Next/)
-      end
-    end
-
-    context 'many users on multiple pages' do
+    context 'for multiple users' do
       before :each do
-        @users= [ FactoryGirl.build_stubbed(:user),
-                  FactoryGirl.build_stubbed(:user, user_name: 'Joseph Soap',
-                    email: 'joe.soap@example.com'),
-                  FactoryGirl.build_stubbed(:user, user_name: 'Tom Jones',
-                    email: 'tom.jones@example.com') ].paginate(:page => 2, :per_page => 2)
+        @users= build_stubbed_list(:user, 2).paginate(:page => 1)
       end
 
-      it 'displays multiple users' do
+      it 'shows the table' do
         render
 
-        expect(rendered).to have_selector('table tbody tr', count: 1)
-        expect(rendered).to have_link('Tom Jones', href: hq_user_path(@users.first))
-        expect(rendered).to match 'tom.jones@example.com'
-        expect(rendered).to_not have_link('Joseph Soap')
-        expect(rendered).to_not match 'joe.soap@example.com'
+        expect(rendered).to_not match /No Users Found/
+        expect(rendered).to have_selector('table tbody tr', count: 2)
       end
 
-      it 'shows the pagination bar' do
+      it 'shows user names as link' do
         render
 
-        expect(rendered).to have_selector('.pagination', text: /Previous(.*)1(.*)Next/)
-      end
-
-      it 'paginates' do
-        expect(view).to receive(:will_paginate)
-
-        render
+        expect(rendered).to have_link(@users.first.user_name, href: hq_user_path(@users.first))
       end
     end
   end
