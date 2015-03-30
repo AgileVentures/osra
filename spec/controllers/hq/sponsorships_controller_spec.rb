90178ee1f5331e7ffc3b10c75f7ed9ee3ae1dc96
Razor <razorcd@yahoo.com>
2015-03-30 17:31:31 +0300

    added successful and unsuccessful tests to sponsorships#inactivate and sponsorships#destroy actions

--- spec/controllers/hq/sponsorships_controller_spec.rb
+++ spec/controllers/hq/sponsorships_controller_spec.rb
@@ -7,21 +7,43 @@ RSpec.describe Hq::SponsorshipsController, type: :controller do
     sign_in instance_double(AdminUser)
   end
 
-  specify "#inactivate" do
-    expect(Sponsorship).to receive(:find).and_return(sponsorship)
-    expect(sponsorship).to receive(:update!)
-    put :inactivate, id: sponsorship.id, sponsorship: {end_date: Date.current}
+  context "#inactivate" do
+    specify "successful" do
+      expect(Sponsorship).to receive(:find).and_return(sponsorship)
+      expect(sponsorship).to receive(:update!)
+      put :inactivate, id: sponsorship.id, sponsorship: {end_date: Date.current}
 
-    expect(response).to redirect_to hq_sponsor_path(sponsorship.sponsor)
-    expect(flash[:success]).to_not be_nil
+      expect(response).to redirect_to hq_sponsor_path(sponsorship.sponsor)
+      expect(flash[:success]).to_not be_nil
+    end
+
+    specify "unsuccessful" do
+      expect(Sponsorship).to receive(:find).and_return(sponsorship)
+      expect(sponsorship).to receive(:update!).and_raise
+      put :inactivate, id: sponsorship.id, sponsorship: {end_date: Date.current}
+
+      expect(response).to redirect_to hq_sponsor_path(sponsorship.sponsor)
+      expect(flash[:error]).to_not be_nil
+    end
   end
 
-  specify "#destroy" do
-    expect(Sponsorship).to receive(:find).and_return(sponsorship)
-    expect(sponsorship).to receive(:destroy!)
-    delete :destroy, id: sponsorship.id
+  context "#destroy" do
+    specify "successful" do
+      expect(Sponsorship).to receive(:find).and_return(sponsorship)
+      expect(sponsorship).to receive(:destroy!)
+      delete :destroy, id: sponsorship.id
 
-    expect(response).to redirect_to hq_sponsor_path(sponsorship.sponsor)
-    expect(flash[:success]).to_not be_nil
+      expect(response).to redirect_to hq_sponsor_path(sponsorship.sponsor)
+      expect(flash[:success]).to_not be_nil
+    end
+
+    specify "unsuccessful" do
+      expect(Sponsorship).to receive(:find).and_return(sponsorship)
+      expect(sponsorship).to receive(:destroy!).and_raise
+      delete :destroy, id: sponsorship.id
+
+      expect(response).to redirect_to hq_sponsor_path(sponsorship.sponsor)
+      expect(flash[:error]).to_not be_nil
+    end
   end
 end
