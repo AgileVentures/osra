22184ca00ee03735cba4ad8f2c840eddb04f914a
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2015-04-10 15:17:50 +0300

    Place new label_for_field method in a module, which is then included into AV::Helpers::FormBuilder

--- config/initializers/form_builder.rb
+++ config/initializers/form_builder.rb
@@ -1,18 +1,23 @@
-class ActionView::Helpers::FormBuilder
+module RailsExtensions
+  module FormBuilder
+    module RequiredFields
+      def label_for_field(method, text = nil, options = {}, &block)
+        if presence_validated? method
+          options.merge!({ :class => 'required_field' })
+        end
 
-  def label_for_field(method, text = nil, options = {}, &block)
-    if presence_validated? method
-      options.merge!({ :class => 'required_field' })
+        self.label(method, text, options, &block)
+      end
+
+      private
+
+      def presence_validated?(attr)
+        attr_validators = object.class.validators_on(attr).map(&:class)
+
+        (PRESENCE_VALIDATORS & attr_validators).present?
+      end
     end
-
-    self.label(method, text, options, &block)
-  end
-
-  private
-
-  def presence_validated?(attr)
-    attr_validators = object.class.validators_on(attr).map(&:class)
-
-    (PRESENCE_VALIDATORS & attr_validators).present?
   end
 end
+
+ActionView::Helpers::FormBuilder.include RailsExtensions::FormBuilder::RequiredFields
