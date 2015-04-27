3de495eb18aa0996889ceeb0a6f68e6aca98be52
Razor <razorcd@yahoo.com>
2015-04-27 18:10:41 +0300

    fixed migration for missing_method on orphan_status and orphan_sponsorship_status, after deleting models

--- db/migrate/20150226095809_add_status_enums_to_orphan.rb
+++ db/migrate/20150226095809_add_status_enums_to_orphan.rb
@@ -15,16 +15,24 @@ class AddStatusEnumsToOrphan < ActiveRecord::Migration
   private
 
   def set_status
+    connection = ActiveRecord::Base.connection
+    orphan_statuses = connection.execute("SELECT * FROM orphan_statuses")
+
     Orphan.all.each do |orphan|
-      status_code = orphan.orphan_status.code
+      orphan_status = orphan_statuses.select {|os| os["id"] == orphan.orphan_status_id.to_s}.first
+      status_code = orphan_status["code"].to_i
       orphan.update_column(:status, ( status_code - 1 ))
     end
   end
 
   def set_sponsorship_status
+    connection = ActiveRecord::Base.connection
+    orphan_sponsorship_statuses = connection.execute("SELECT * FROM orphan_sponsorship_statuses")
+
     Orphan.all.each do |orphan|
-      status_code = orphan.orphan_sponsorship_status.code
-      orphan.update_column(:sponsorship_status, ( status_code - 1 ))
+      orphan_sponsorship_status = orphan_sponsorship_statuses.select {|oss| oss["id"] == orphan.orphan_sponsorship_status_id.to_s}.first
+      sponsorship_status_code = orphan_sponsorship_status["code"].to_i
+      orphan.update_column(:sponsorship_status, ( sponsorship_status_code - 1 ))
     end
   end
 end
