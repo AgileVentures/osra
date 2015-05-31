f99d0941cb78ec94dbdf556370ce262110929f8f
evo2013 <CVMailer_2011@yahoo.com>
2015-05-31 01:14:09 -0700

    sort table headers hq_partners

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,13 @@ module ApplicationHelper
   FULL_DATE_FORMAT_STRING= '%d %B %Y'
   MONTH_YEAR_DATE_FORMAT_STRING= '%m/%Y'
 
+  def sortable(column, title = nil)
+  	title ||= column.titleize
+  	css_class = column == sort_column ? "current #{sort_direction}" : nil
+  	direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+  	link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end
+
   def format_full_date(date)
     (date || NullDate.new).strftime(FULL_DATE_FORMAT_STRING)
   end
