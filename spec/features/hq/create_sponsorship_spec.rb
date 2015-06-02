bae573b51eee499bac6864032a61c37905cd7750
Gabriel Muñumel <gabrielmunumel@gmail.com>
2015-06-03 00:29:36 +0100

    Added suggested changes

--- spec/features/hq/create_sponsorship_spec.rb
+++ spec/features/hq/create_sponsorship_spec.rb
@@ -11,28 +11,31 @@ RSpec.feature 'Create a sponsorship', :type => :feature do
 
   scenario 'Pairing a sponsor with orphans' do
     when_i_visit_show_sponsor "First Sponsor"
-    and_i_should_be_on "hq_new_sponsorship_path", "First Sponsor"
+    and_i_should_be_on "hq_new_sponsorship_path", {sponsor_name: "First Sponsor"}
     and_i_should_see "First Sponsor"
     and_i_should_see "First Orphan"
     and_i_should_see "Second Orphan"
     fill_orphan_with "sponsorship_start_date", "First Orphan", "2014-01-01"
     when_i_click "Sponsor this orphan", "First Orphan"
-    and_i_should_be_on "hq_new_sponsorship_path", "First Sponsor"
+    and_i_should_be_on "hq_new_sponsorship_path", {sponsor_name: "First Sponsor"}
     and_i_should_see "Return to Sponsor Page"
     and_i_should_see "No (1/2)"
     and_i_should_see "Sponsorship link was successfully created for First Orphan"
+    fill_orphan_with "sponsorship_start_date", "Second Orphan", "1900-02-01"
+    when_i_click "Sponsor this orphan", "Second Orphan"
+    and_i_should_see "Validation failed"
     fill_orphan_with "sponsorship_start_date", "Second Orphan", "2014-02-01"
     when_i_click "Sponsor this orphan", "Second Orphan"
-    and_i_should_be_on "hq_sponsor_path", "First Sponsor"
+    and_i_should_be_on "hq_sponsor_path", {sponsor_name: "First Sponsor"}
     and_i_should_see_within "Currently Sponsored Orphans", "First Orphan"
     and_i_should_see "2014-01-01"
     and_i_should_see_within "Currently Sponsored Orphans", "Second Orphan"
+    and_i_should_see "Sponsorship link was successfully created."
   end
 
   scenario 'Sponsor on hold cannot create sponsorships' do
     given_sponsor_on_hold "Second Sponsor"
     visit_sponsor "Second Sponsor"
-    and_i_should_not_see "Link to Orphan"
     and_i_should_not_see "First Orphan"
     and_i_should_not_see "Second Orphan"
   end
@@ -40,7 +43,6 @@ RSpec.feature 'Create a sponsorship', :type => :feature do
   scenario 'Inactive sponsor cannot create sponsorships' do
     given_inactive_sponsor "Second Sponsor"
     visit_sponsor "Second Sponsor"
-    and_i_should_not_see "Link to Orphan"
     and_i_should_not_see "First Orphan"
     and_i_should_not_see "Second Orphan"
   end
--- spec/support/feature_helpers/navigation.rb
+++ spec/support/feature_helpers/navigation.rb
@@ -1,13 +1,27 @@
 module FeatureHelpers
   module Navigation
-    def and_i_should_be_on page, sponsor_name = ''
-      sponsor = Sponsor.find_by(name: sponsor_name) if not sponsor_name.empty?
+    def and_i_should_be_on page, field_hash = {}
+      if field_hash.empty?
+        get_path page
+        return
+      end
+      case field_hash.keys.first.to_sym
+        when :sponsor_name
+          sponsor = Sponsor.find_by(name: field_hash[:sponsor_name])
+          get_path page, sponsor.id
+        when :sponsor_id
+          get_path page, field_hash[:sponsor_id]
+        else raise('path to specified object is not displayed')
+      end
+    end
+
+    def get_path page, object_id = ''
       page_path = case page.to_sym
                     when :root_page then hq_root_path
                     when :sign_in_page then new_hq_admin_user_session_path
                     when :new_sponsor_page then new_hq_sponsor_path
-                    when :hq_sponsor_path then hq_sponsor_path sponsor.id
-                    when :hq_new_sponsorship_path then hq_new_sponsorship_path sponsor.id
+                    when :hq_sponsor_path then hq_sponsor_path object_id
+                    when :hq_new_sponsorship_path then hq_new_sponsorship_path object_id
                     else raise('path to specified is not listed in #path_to')
                   end
 
