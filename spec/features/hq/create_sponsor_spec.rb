b73e660e702e49f9ffcfd6a3f8378b6547514f0b
chock <aaron.chock@gmail.com>
2015-06-05 16:42:42 -0600

    adding agenthelper

--- spec/features/hq/create_sponsor_spec.rb
+++ spec/features/hq/create_sponsor_spec.rb
@@ -4,14 +4,14 @@ RSpec.feature 'User enters new sponsor data', :type => :feature do
 
   background do
     i_sign_in_as_admin
+    an_agent_exists_with_username
   end
 
   scenario 'User clicks the New Sponsor button' do
     when_i_register_new_sponsor
-    and_i_click_button 'Create Sponsor'
 
     then_the_sponsor_should_be_saved
-    and_i_should_be_on :new_sponsor_page
+    and_i_should_be_on :hq_sponsor_path
   end
 
   def when_i_register_new_sponsor
@@ -20,14 +20,15 @@ RSpec.feature 'User enters new sponsor data', :type => :feature do
     mDate = t.strftime("%Y-%m-%d")
     fill_in 'Name', with: 'Jack Napier'
     select 'Active', from: 'sponsor[status_id]'
-    select 'Male', from: 'sponsor[gender]'
-    fill_in 'Start date', with: '2015-06-01'
+    select 'Female', from: 'sponsor[gender]'
+    fill_in 'Start date', with: mDate
     fill_in 'Requested orphan count', with: '1'
     select 'Organization', from: 'sponsor[sponsor_type_id]'
     select 'أهل الغربة وقت الكربة', from: 'sponsor[organization_id]'
     select '(Afghanistan) أفغانستان', from: 'sponsor[country]'
-    select '**Add New**', from: 'sponsor[city]'
+    select 'Sigurdland', from: 'sponsor[city]'
     select 'Trisha Marquardt 1', from: 'sponsor[agent_id]'
+    click_button 'Create Sponsor'
   end
 
   def then_the_sponsor_should_be_saved
