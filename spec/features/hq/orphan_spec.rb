4a84a34681d5f3e9d754f4460d276306ca9040bd
Zmago Devetak <zmago_devetak@yahoo.com>
2015-06-19 11:12:18 +0000

    change orphan id

--- spec/features/hq/orphan_spec.rb
+++ spec/features/hq/orphan_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature 'Orphan CRUD spec:', :type => :feature do
 
   background do
     i_sign_in_as_admin
-    an_orphan_exists 13, "Orphan 1", "Father 1", "2012-01-01", "Male", "Normal"
+    an_orphan_exists 1, "Orphan 1", "Father 1", "2012-01-01", "Male", "Normal"
     an_orphan_exists 2, "Orphan 2", "Father 2", "2011-01-01", "Female", "High"
   end
   
@@ -12,9 +12,9 @@ RSpec.feature 'Orphan CRUD spec:', :type => :feature do
     visit hq_orphans_path
     and_i_should_see "Orphan 1 Father 1"
     and_i_should_see "Orphan 2 Father 2"
-    and_i_should_see_within "Orphan 13", "2012-01-01"
+    and_i_should_see_within "Orphan 1", "2012-01-01"
     and_i_should_see_within "Orphan 2", "High"
-    and_i_should_see_within "Orphan 13", "Male"
+    and_i_should_see_within "Orphan 1", "Male"
     and_i_should_see_within "Orphan 2", "Female"
   end
   
@@ -27,13 +27,13 @@ RSpec.feature 'Orphan CRUD spec:', :type => :feature do
   scenario 'Should be able to visit an orphan from the orphan index page' do
     visit hq_orphans_path
     when_i_click "Orphan 1", "Orphan 1"
-    and_i_should_be_on "hq_orphan_path", { orphan_id: 13 }
+    and_i_should_be_on "hq_orphan_path", { orphan_id: 1 }
   end
   
   scenario 'Should be able to edit an orphan from the orphan show page' do
     visit_orphan( "Orphan 1" )
     and_i_click_link "Edit Orphan"
-    and_i_should_be_on "edit_hq_orphan_path", { orphan_id: 13 }
+    and_i_should_be_on "edit_hq_orphan_path", { orphan_id: 1 }
     expect(page).to_not have_selector '#osra_num'
     expect(page).to have_css '#orphan_sponsorship_status[disabled]'
     fill_in 'orphan[name]', with: 'Orphan N'
@@ -54,7 +54,7 @@ RSpec.feature 'Orphan CRUD spec:', :type => :feature do
     fill_in 'orphan[alt_contact_number]', with: '000999'
     fill_in 'orphan_original_address_attributes_city', with: 'Rome'
     and_i_click_button "Update Orphan"
-    and_i_should_be_on "hq_orphan_path", { orphan_id: 13 }
+    and_i_should_be_on "hq_orphan_path", { orphan_id: 1 }
     and_i_should_see "Orphan successfuly saved"
     and_i_should_see "Orphan N"
     and_i_should_see "01 January 2010"
@@ -74,7 +74,7 @@ RSpec.feature 'Orphan CRUD spec:', :type => :feature do
   scenario 'Orphan cannot be older than 22 years of age' do
     visit_orphan( "Orphan 1" )
     and_i_click_link "Edit Orphan"
-    and_i_should_be_on "edit_hq_orphan_path", { orphan_id: 13 }
+    and_i_should_be_on "edit_hq_orphan_path", { orphan_id: 1 }
     fill_in 'orphan[date_of_birth]', with: '1950-01-01'
     and_i_click_button "Update Orphan"
     and_i_should_see "Date of birth Orphan must be younger than 22 years old to join OSRA."  
@@ -83,10 +83,10 @@ RSpec.feature 'Orphan CRUD spec:', :type => :feature do
   scenario 'Should return to orphan show page if editing an orphan is cancelled' do
     visit_orphan( "Orphan 1" )
     and_i_click_link "Edit Orphan"
-    and_i_should_be_on "edit_hq_orphan_path", { orphan_id: 13 }
+    and_i_should_be_on "edit_hq_orphan_path", { orphan_id: 1 }
     fill_in 'orphan[name]', with: 'Orphan N'
     and_i_click_link "Cancel"
-    and_i_should_be_on "hq_orphan_path", { orphan_id: 13 }
+    and_i_should_be_on "hq_orphan_path", { orphan_id: 1 }
   end
   
   def when_i_click( link, orphan_name )
