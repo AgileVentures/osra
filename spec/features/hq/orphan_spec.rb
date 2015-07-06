f07e3ca45b9328dcf77ab74d171eab9a86d90573
Zmago Devetak <zmago_devetak@yahoo.com>
2015-07-06 07:51:53 +0000

    added fields for edit orphan spec

--- spec/features/hq/orphan_spec.rb
+++ spec/features/hq/orphan_spec.rb
@@ -10,6 +10,12 @@ RSpec.feature 'Orphan CRUD spec:', :type => :feature do
   
   scenario 'There should be a list of orphans on the admin index page' do
     visit hq_orphans_path
+    and_i_should_see "Orphan 1"
+    and_i_should_see_within "Orphan 1", "Father 1"
+    and_i_should_see_within "Orphan 1", "2012-01-01"
+    and_i_should_see "Orphan 2"
+    and_i_should_see_within "Orphan 2", "Father 2"
+    and_i_should_see_within "Orphan 2", "2011-01-01"
     and_i_should_see "Orphan 1 Father 1"
     and_i_should_see "Orphan 2 Father 2"
     and_i_should_see_within "Orphan 1", "2012-01-01"
@@ -41,29 +47,74 @@ RSpec.feature 'Orphan CRUD spec:', :type => :feature do
     fill_in 'orphan[father_given_name]', with: 'Father N'
     select "Female", :from => 'orphan[gender]'
     fill_in 'orphan[health_status]', with: 'Good'
+    fill_in 'orphan[schooling_status]', with: 'goes to school'
     check 'orphan[goes_to_school]'
-    select 'Inactive', :from => 'orphan[status]'
+    select 'Active', :from => 'orphan[status]'
     select 'High', :from => 'orphan[priority]'
+    fill_in 'orphan[father_given_name]', with: 'Orphan Nxy'
     fill_in 'orphan[family_name]', with: 'Asdf'
+    check 'orphan[father_deceased]'
     fill_in 'orphan[mother_name]', with: 'Aisha'
     check 'orphan[mother_alive]'
+    check 'orphan[father_is_martyr]'
     fill_in 'orphan[father_occupation]', with: 'Merchant'
+    fill_in 'orphan[father_place_of_death]', with: 'Gaza'
+    fill_in 'orphan[father_cause_of_death]', with: 'natural'
+    fill_in 'orphan[father_date_of_death]', with: '2015-06-01'
     fill_in 'orphan[guardian_name]', with: 'Goku'
-    fill_in 'orphan[guardian_id_num]', with: '999'
-    fill_in 'orphan[guardian_name]', with: '999.999.999'
+    fill_in 'orphan[guardian_relationship]', with: 'sister'
+    fill_in 'orphan[guardian_id_num]', with: '123'
+    fill_in 'orphan[contact_number]', with: '321123'
     fill_in 'orphan[alt_contact_number]', with: '000999'
-    fill_in 'orphan_original_address_attributes_city', with: 'Rome'
+    fill_in 'orphan[original_address_attributes][city]', with: 'Rome'
+    select 'Hama', :from => 'orphan[original_address_attributes][province_id]'
+    fill_in 'orphan[original_address_attributes][neighborhood]', with: 'neighborhood'
+    fill_in 'orphan[original_address_attributes][street]', with: 'infinite loop 1'
+    fill_in 'orphan[original_address_attributes][details]', with: 'Lorem Ipsum'
+    fill_in 'orphan[current_address_attributes][city]', with: 'New Town'
+    select 'Homs', :from => 'orphan[current_address_attributes][province_id]'
+    fill_in 'orphan[current_address_attributes][neighborhood]', with: 'NewTownHood'
+    fill_in 'orphan[current_address_attributes][street]', with: 'NewTownStreet'
+    fill_in 'orphan[current_address_attributes][details]', with: 'NewTown details'
+    check 'orphan[sponsored_by_another_org]'
+    fill_in 'orphan[minor_siblings_count]', with: '10'
+    fill_in 'orphan[sponsored_minor_siblings_count]', with: '5'
+    fill_in 'orphan[comments]', with: 'This is a comment'
     and_i_click_button "Update Orphan"
     and_i_should_be_on "hq_orphan_path", { orphan_id: 1 }
     and_i_should_see "Orphan successfuly saved"
     and_i_should_see "Orphan N"
     and_i_should_see "01 January 2010"
+    and_i_should_see 'Good'
+    and_i_should_see 'goes to school'
+    and_i_should_see 'Active'
+    and_i_should_see 'High'
+    and_i_should_see 'Orphan Nxy'
     and_i_should_see 'Asdf'
     and_i_should_see 'Aisha'
     and_i_should_see 'Merchant'
+    and_i_should_see 'Gaza'
+    and_i_should_see 'natural'
+    and_i_should_see '01 June 2015'
+    and_i_should_see 'Goku'
+    and_i_should_see 'sister'
     and_i_should_see 'Rome'
-    and_i_should_see '999'
+    and_i_should_see '123'
+    and_i_should_see '321123'
     and_i_should_see '000999'
+    and_i_should_see 'Hama'
+    and_i_should_see 'neighborhood'
+    and_i_should_see 'infinite loop 1'
+    and_i_should_see 'Lorem Ipsum'
+    and_i_should_see 'New Town'
+    and_i_should_see 'Homs'
+    and_i_should_see 'NewTownHood'
+    and_i_should_see 'NewTownStreet'
+    and_i_should_see 'NewTown details'
+    and_i_should_see 'Sponsored by Another Org. Yes'
+    and_i_should_see '10'
+    and_i_should_see '5'
+    and_i_should_see 'This is a comment'
   end
   
   scenario 'Should not be able to delete an orphan from the orphan show page' do
