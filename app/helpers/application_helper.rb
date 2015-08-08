f920900f044624414cccadc7afb726a356412f5b
razor <razorcd@yahoo.com>
2015-08-09 01:01:40 +0300

    adds css sorting arrows

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,14 +20,23 @@ module ApplicationHelper
     options[:table_header] ||= sort_column.titleize
 
     link_direction = nil
-    icon = ''
 
     if options[:sort_column_is_active]
       link_direction = invert_sort_direction_of(options[:sort_direction])
       icon = "<span class=\"glyphicon th_sort_#{link_direction}\"></span>"
+    else
+      icon = "<div class='asc_desc'>" +
+              "<span class=\"glyphicon th_sort_asc\"></span>" +
+              "<span class=\"glyphicon th_sort_desc\"></span>" +
+             "</div>"
     end
 
-    th_link = link_to options[:table_header], {:sort_column => sort_column, :sort_direction => link_direction, :sort_columns_included_resource => options[:sort_columns_included_resource]}
+    query_params = request.query_parameters.merge({
+      :sort_column => sort_column,
+      :sort_direction => link_direction,
+      :sort_columns_included_resource => options[:sort_columns_included_resource]
+    })
+    th_link = link_to options[:table_header], query_params
 
     return (th_link + icon.html_safe)
   end
