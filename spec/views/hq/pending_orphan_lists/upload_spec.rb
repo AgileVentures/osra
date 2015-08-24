e85046ed487da9286b06a7dfbc6d4e270c074aef
razor <razorcd@yahoo.com>
2015-08-25 00:47:36 +0300

    upload view should delegate to partials

--- spec/views/hq/pending_orphan_lists/upload_spec.rb
+++ spec/views/hq/pending_orphan_lists/upload_spec.rb
@@ -7,12 +7,19 @@ RSpec.describe "hq/pending_orphan_lists/upload.html.erb", type: :view do
     assign :partner, partner
   end
 
+  it 'should delegate to partials' do
+    render
+
+    expect(view).to render_template partial: 'layouts/_content_title'
+    expect(view).to render_template partial: 'shared/_errors'
+  end
+
   it 'shows Spredsheet upload form' do
     render
 
     expect(rendered).to have_selector("input[type='file']")
     expect(rendered).to have_selector("input[type='submit'][value='Upload']")
-    expect(rendered).to have_link("Cancel")
+    expect(rendered).to have_link("Cancel", hq_partner_path(partner))
   end
 
 end
