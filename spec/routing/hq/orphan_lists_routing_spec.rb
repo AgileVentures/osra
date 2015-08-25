334cfd902e1d47150f00191976f6f537fd983c55
razor <razorcd@yahoo.com>
2015-08-25 03:41:14 +0300

    orphan_list#index route spec

--- spec/routing/hq/orphan_lists_routing_spec.rb
+++ spec/routing/hq/orphan_lists_routing_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Hq::Orphan Lists routing', type: :routing do
-  specify { pending('method-missing Hq::OrphanListsController#index')
-            expect(get: '/hq/partners/1/orphan_lists').           to be_routable } #index
+  specify { expect(get: '/hq/partners/1/orphan_lists').           to be_routable } #index
   specify { expect(post: '/hq/partners/1/orphan_lists').      not_to be_routable } #create
   specify { expect(get: '/hq/partners/1/orphan_lists/new').   not_to be_routable } #new
   specify { expect(get: '/hq/partners/1/orphan_lists/1').     not_to be_routable } #show
