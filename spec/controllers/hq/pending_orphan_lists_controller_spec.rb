8202317fff94b81e37b8bcca8fa53cf8289a00ea
razor <razorcd@yahoo.com>
2015-08-27 22:24:52 +0300

    validate controller action specs

--- spec/controllers/hq/pending_orphan_lists_controller_spec.rb
+++ spec/controllers/hq/pending_orphan_lists_controller_spec.rb
@@ -1,19 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Hq::PendingOrphanListsController, type: :controller do
-  let(:partner) { instance_double Partner }
+  let(:partner) { build_stubbed :partner }
+  let(:pending_orphan_list) { instance_double PendingOrphanList, spreadsheet: 'sheet', destroy!: true }
+  let(:orphan_importer) { double }
+  # let(:orphan_lists) { double }
+  # let(:orphan_list) { double }
+  # let(:pending_orphans) { double }
+
+
 
   before :each do
     sign_in instance_double(AdminUser)
 
-    allow(Partner).to receive(:find).with('1').and_return partner
+    allow(Partner).to receive(:find).with(partner.id.to_s).and_return partner
   end
 
   describe 'upload' do
     context 'when partner is inactive' do
       it 'redirects to partner if partner not active' do
         expect(partner).to receive(:active?).and_return false
-        get :upload, partner_id: 1
+        get :upload, partner_id: partner.id
 
         expect(response).to redirect_to hq_partner_path(partner)
       end
@@ -23,7 +30,7 @@ RSpec.describe Hq::PendingOrphanListsController, type: :controller do
       before(:each) do
         expect(partner).to receive(:active?).and_return true
 
-        get :upload, partner_id: 1
+        get :upload, partner_id: partner.id
       end
 
       it 'sets instance variables' do
@@ -32,6 +39,56 @@ RSpec.describe Hq::PendingOrphanListsController, type: :controller do
 
       it 'renders :upload' do
         expect(response).to render_template :upload
+      end
+    end
+  end
+
+  describe 'validate' do
+    context 'when partner is inactive' do
+      it 'redirects to partner if partner not active' do
+        expect(partner).to receive(:active?).and_return false
+        post :validate, partner_id: partner.id
+
+        expect(response).to redirect_to hq_partner_path(partner.id)
+      end
+    end
+
+    context 'when no spreadsheet is specified' do
+
+      before do
+        allow(partner).to receive(:active?).and_return true
+        post :validate, partner_id: partner.id
+      end
+
+      it 'redirects back to the current page' do
+        expect(response).to redirect_to upload_hq_partner_pending_orphan_lists_path(partner.id)
+      end
+
+    end
+
+    context 'when partner is active' do
+      let(:orphan_list_params) { { spreadsheet: fixture_file_upload('one_orphan_xls.xls') } }
+
+      before do
+        allow(partner).to receive(:active?).and_return true
+        allow(pending_orphan_list).to receive :save!
+        allow(pending_orphan_list).to receive :pending_orphans=
+        allow(orphan_importer).to receive :extract_orphans
+        allow(orphan_importer).to receive(:valid?).and_return true
+        post :validate, partner_id: partner.id, pending_orphan_list: orphan_list_params
+      end
+
+      it 'sets instance variables' do
+        expect(assigns :partner).to eq partner
+        expect(assigns :pending_orphan_list).to eq pending_orphan_list
+      end
+
+      it 'saves pending_orphan_list' do
+        expect(pending_orphan_list).to have_received :save!
+      end
+
+      it 'renders :validate' do
+        expect(response).to render_template :valid_list
       end
     end
   end
