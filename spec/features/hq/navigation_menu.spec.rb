b9fbb9113ae5d53b8bbb1a40bcb1be0dc5183762
Craig Ferry <PurityControl@users.noreply.github.com>
2015-08-31 00:56:26 -0700

    remove inappropriate whitespace

--- spec/features/hq/navigation_menu.spec.rb
+++ spec/features/hq/navigation_menu.spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.feature 'Navigation menu', :type => :feature do
-    
+
   background do
     i_sign_in_as_admin
   end
@@ -17,38 +17,38 @@ RSpec.feature 'Navigation menu', :type => :feature do
     within "#main_nav_bar" do
       click_link "Dashboard"
     end
-    
+
     expect(page.current_path).to eq hq_dashboard_index_path
   end
-  
+
   scenario 'user click on Admin Users link' do
     within "#main_nav_bar" do
       click_link "Admin User"
     end
     expect(page.current_path).to eq hq_admin_users_path
   end
-  
+
   scenario 'user click on Orphans link' do
     within "#main_nav_bar" do
       click_link "Orphans"
     end
     expect(page.current_path).to eq hq_orphans_path
   end
-  
+
   scenario 'user click on Partners link' do
     within "#main_nav_bar" do
       click_link "Partners"
     end
     expect(page.current_path).to eq hq_partners_path
   end
-  
+
   scenario 'user click on Sponsors' do
     within "#main_nav_bar" do
       click_link "Sponsors"
     end
     expect(page.current_path).to eq hq_sponsors_path
   end
-  
+
   scenario 'user click on Users' do
     within "#main_nav_bar" do
       click_link "Users"
