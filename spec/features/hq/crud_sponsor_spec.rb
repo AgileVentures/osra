4ee3f7692fb3eeff6d588a7fb0d92a6ab32e2f28
Craig Ferry <PurityControl@users.noreply.github.com>
2015-09-02 09:20:12 -0700

    remove given_i_am_on_age_for_sponsor and use get_path feature helper

--- spec/features/hq/crud_sponsor_spec.rb
+++ spec/features/hq/crud_sponsor_spec.rb
@@ -11,6 +11,7 @@ RSpec.feature 'CRUD Sponsor', :type => :feature do
 
   background do
     @sponsors = create_list :sponsor_full, 3
+    @sponsor = @sponsors.first
     i_sign_in_as_admin
   end
 
@@ -30,41 +31,39 @@ RSpec.feature 'CRUD Sponsor', :type => :feature do
   end
 
   scenario 'I should see the required fields on the sponsor show page' do
-    sponsor = @sponsors.first
-    given_i_am_on_page_for_sponsor 'Show Sponsor', sponsor.name
+    visit(get_path(:hq_sponsor_page, @sponsor.id))
     i_should_see_the_following_fields_on_the_page [
-      { field: 'name',                   value: sponsor.name },
-      { field: 'osra_num',               value: sponsor.osra_num },
-      { field: 'status',                 value: sponsor.status.name },
-      { field: 'gender',                 value: sponsor.gender },
-      { field: 'start_date',             value: format_date(sponsor.start_date) },
-      { field: 'requested_orphan_count', value: sponsor.requested_orphan_count },
-      { field: 'request_fulfilled',      value: bool_to_s(sponsor.request_fulfilled) },
-      { field: 'payment_plan',           value: sponsor.payment_plan },
-      { field: 'sponsor_type',           value: sponsor.sponsor_type.name },
-      { field: 'affiliate',              value: sponsor.affiliate },
-      { field: 'country',                value: en_ar_country(sponsor.country) },
-      { field: 'city',                   value: sponsor.city },
-      { field: 'address',                value: sponsor.address },
-      { field: 'email',                  value: sponsor.email },
-      { field: 'contact1',               value: sponsor.contact1 },
-      { field: 'contact2',               value: sponsor.contact2 },
-      { field: 'additiona_info',         value: sponsor.additional_info },
-      { field: 'agent',                  value: sponsor.agent.user_name },
-      { field: 'created_at',             value: format_date(sponsor.created_at) },
-      { field: 'updated_at',             value: format_date(sponsor.updated_at) }
+      { field: 'name',                   value: @sponsor.name },
+      { field: 'osra_num',               value: @sponsor.osra_num },
+      { field: 'status',                 value: @sponsor.status.name },
+      { field: 'gender',                 value: @sponsor.gender },
+      { field: 'start_date',             value: format_date(@sponsor.start_date) },
+      { field: 'requested_orphan_count', value: @sponsor.requested_orphan_count },
+      { field: 'request_fulfilled',      value: bool_to_s(@sponsor.request_fulfilled) },
+      { field: 'payment_plan',           value: @sponsor.payment_plan },
+      { field: 'sponsor_type',           value: @sponsor.sponsor_type.name },
+      { field: 'affiliate',              value: @sponsor.affiliate },
+      { field: 'country',                value: en_ar_country(@sponsor.country) },
+      { field: 'city',                   value: @sponsor.city },
+      { field: 'address',                value: @sponsor.address },
+      { field: 'email',                  value: @sponsor.email },
+      { field: 'contact1',               value: @sponsor.contact1 },
+      { field: 'contact2',               value: @sponsor.contact2 },
+      { field: 'additiona_info',         value: @sponsor.additional_info },
+      { field: 'agent',                  value: @sponsor.agent.user_name },
+      { field: 'created_at',             value: format_date(@sponsor.created_at) },
+      { field: 'updated_at',             value: format_date(@sponsor.updated_at) }
     ]
   end
 
   scenario 'Should be able to visit a sponsor from the sponsor index page' do
-    sponsor = @sponsors.first
     visit hq_sponsors_path
-    click_link sponsor.name
-    and_i_should_be_on :hq_sponsor_page, { sponsor_name: sponsor.name }
+    click_link @sponsor.name
+    and_i_should_be_on :hq_sponsor_page, { sponsor_name: @sponsor.name }
   end
 
   scenario 'Should be able to add a sponsor from the sponsor index page' do
-    given_i_am_on_page_for_sponsor 'New Sponsor'
+    visit(get_path(:new_sponsor_page))
     fill_in 'Name', :with => 'Sponsor1'
     select 'Active', :from => 'Status'
     select 'Male', :from => 'Gender'
@@ -82,36 +81,34 @@ RSpec.feature 'CRUD Sponsor', :type => :feature do
   end
 
   scenario 'Should be able to edit a sponsor from the sponsor show page' do
-    name = @sponsors.first.name
-    given_i_am_on_page_for_sponsor 'Show Sponsor', name
+    visit(get_path(:hq_sponsor_page, @sponsor.id))
     click_link 'Edit Sponsor'
-    and_i_should_be_on :edit_hq_sponsor_page, { sponsor_name: name }
+    and_i_should_be_on :edit_hq_sponsor_page, { sponsor_name: @sponsor.name }
     fill_in 'Additional info', :with => 'Additional Information'
     click_button 'Update Sponsor'
-    and_i_should_be_on :hq_sponsor_page, { sponsor_name: name }
+    and_i_should_be_on :hq_sponsor_page, { sponsor_name: @sponsor.name }
     and_i_should_see 'Sponsor successfuly saved'
     and_i_should_see 'Additional Information'
   end
 
   scenario "can't edit sponsor_type, organization or branch" do
-    given_i_am_on_page_for_sponsor 'Edit Sponsor', @sponsors.first.name
+    visit(get_path(:edit_hq_sponsor_page, @sponsor.id))
     i_should_not_be_able_to_change 'Sponsor type', 'sponsor'
     i_should_not_be_able_to_change 'Organization', 'sponsor'
     i_should_not_be_able_to_change 'Branch', 'sponsor'
   end
 
   scenario "can't delete a sponsor from the sponsor show page" do
-    given_i_am_on_page_for_sponsor 'Show Sponsor', @sponsors.first.name
+    visit(get_path(:hq_sponsor_page, @sponsor.id))
     and_i_should_not_see 'Delete Sponsor'
   end
 
   scenario "returns to sponsor show page on cancelling edit sponsor" do
-    name = @sponsors.first.name
-    given_i_am_on_page_for_sponsor 'Show Sponsor', name
+    visit(get_path(:hq_sponsor_page, @sponsor.id))
     click_link 'Edit Sponsor'
     fill_in 'Additional info', :with => 'Additional Information'
     click_link 'Cancel'
-    and_i_should_be_on :hq_sponsor_page, { sponsor_name: name }
+    and_i_should_be_on :hq_sponsor_page, { sponsor_name: @sponsor.name }
     and_i_should_not_see 'Additional Information'
   end
 
@@ -120,27 +117,6 @@ RSpec.feature 'CRUD Sponsor', :type => :feature do
   def i_should_see_the_following_fields_on_the_page table
     table.each do |hash|
       expect(page).to have_content(hash[:value])
-    end
-  end
-
-  def given_i_am_on_page_for_sponsor page_name, sponsor_name = nil
-    if sponsor_name.nil?
-      visit path_to_sponsor_role page_name
-    else
-      sponsor = Sponsor.find_by name: sponsor_name unless sponsor_name.nil?
-      visit path_to_sponsor_role page_name, sponsor.id
-    end
-  end
-
-  def path_to_sponsor_role page_name, id = nil
-    name = page_name.downcase
-    case name
-      when 'edit sponsor' then
-        edit_hq_sponsor_path id
-      when 'show sponsor' then
-        hq_sponsor_path id
-      when 'new sponsor' then
-        new_hq_sponsor_path
     end
   end
 
