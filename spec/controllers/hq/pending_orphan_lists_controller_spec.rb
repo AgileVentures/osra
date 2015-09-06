d2f5631b89bf53a2cde88d6f4aca8f85f76b2fb5
razor <razorcd@yahoo.com>
2015-09-06 22:42:11 +0300

    adds valid_list view specs

--- spec/controllers/hq/pending_orphan_lists_controller_spec.rb
+++ spec/controllers/hq/pending_orphan_lists_controller_spec.rb
@@ -4,16 +4,21 @@ RSpec.describe Hq::PendingOrphanListsController, type: :controller do
   let(:partner) { build_stubbed :partner }
   let(:pending_orphan_list) { instance_double PendingOrphanList, spreadsheet: 'sheet', destroy!: true }
   let(:orphan_importer) { double }
-  # let(:orphan_lists) { double }
-  # let(:orphan_list) { double }
-  # let(:pending_orphans) { double }
-
-
+  let(:orphan_lists) { double }
+  let(:orphan_list) { double }
+  let(:pending_orphans) { double }
 
   before :each do
     sign_in instance_double(AdminUser)
 
     allow(Partner).to receive(:find).with(partner.id.to_s).and_return partner
+    allow(PendingOrphanList).to receive(:new).and_return pending_orphan_list
+    allow(PendingOrphanList).to receive(:find).with('1').and_return pending_orphan_list
+    allow(OrphanImporter).to receive(:new).and_return orphan_importer
+
+    allow(partner).to receive(:orphan_lists).and_return orphan_lists
+    allow(orphan_lists).to receive(:create!).and_return orphan_list
+    allow(orphan_lists).to receive(:build).and_return orphan_list
   end
 
   describe 'upload' do
@@ -63,18 +68,17 @@ RSpec.describe Hq::PendingOrphanListsController, type: :controller do
       it 'redirects back to the current page' do
         expect(response).to redirect_to upload_hq_partner_pending_orphan_lists_path(partner.id)
       end
-
     end
 
     context 'when partner is active' do
       let(:orphan_list_params) { { spreadsheet: fixture_file_upload('one_orphan_xls.xls') } }
-      let(:invalid_orphan_list_params) { { spreadsheet: fixture_file_upload('three_invalid_orphans_xlsx.xlsx') } }
+      let(:invalid_orphan_list_params) { { spreadsheet: fixture_file_upload('two_orphans_first_invalid_xlsx.xlsx') } }
 
       before do
         allow(partner).to receive(:active?).and_return true
         allow(pending_orphan_list).to receive :save!
         allow(pending_orphan_list).to receive :pending_orphans=
-        allow(orphan_importer).to receive :extract_orphans
+        allow(orphan_importer).to receive(:extract_orphans)
         allow(orphan_importer).to receive(:valid?).and_return true
       end
 
@@ -98,8 +102,8 @@ RSpec.describe Hq::PendingOrphanListsController, type: :controller do
         end
       end
 
-      context "invalid orphan_list" do
-        before :each do
+      context "invalid orphan_list", focus: true do
+        before do
           post :validate, partner_id: partner.id, pending_orphan_list: invalid_orphan_list_params
         end
 
@@ -111,9 +115,53 @@ RSpec.describe Hq::PendingOrphanListsController, type: :controller do
           expect(response).to render_template :invalid_list
         end
       end
-
-
     end
+  end
+
+  describe 'import' do
+      let(:orphan) { instance_double Orphan, :save! => true }
+      let(:pending_orphan) { instance_double PendingOrphan, :save! => true }
+      let(:orphans_to_import) { [orphan] }
+
+      before do
+        allow(orphan_list).to receive :orphan_count=
+        allow(orphan_list).to receive(:orphans).and_return orphans_to_import
+        allow(orphan_list).to receive :save!
+        allow(orphan_list).to receive :orphan_count
+        allow(pending_orphan_list).to receive :destroy
+        allow(pending_orphans).to receive(:each).and_yield(pending_orphan)
+        allow(orphan_list).to receive :osra_num
+        allow(pending_orphan_list).to receive(:pending_orphans).and_return pending_orphans
+        allow(pending_orphan).to receive(:to_orphan).and_return orphan
+      end
+
+      context 'when orphan records are valid' do
+        before do
+          allow(orphans_to_import).to receive_message_chain(:map, :all?).and_return true
+          post :import, partner_id: partner.id, orphan_list: { pending_id: 1 }
+        end
+
+        it 'sets instance variables' do
+          expect(assigns :partner).to eq partner
+          expect(assigns :pending_orphan_list).to eq pending_orphan_list
+        end
+
+        it 'saves orphan_list' do
+          expect(orphan_list).to (have_received :save!)
+        end
+
+        it 'destroys pending_orphan_list' do
+          expect(pending_orphan_list).to have_received :destroy
+        end
+
+        it 'sets flash message' do
+          expect(flash[:notice]).to include('was successfully imported.')
+        end
+
+        it 'redirects to partner' do
+          expect(response).to redirect_to hq_partner_path(partner)
+        end
+      end
   end
 
 end
