955fc397d5f2efdcc464c34c15ea580bc4e956d0
razor <razorcd@yahoo.com>
2015-09-07 02:16:14 +0300

    destroy spec

--- spec/controllers/hq/pending_orphan_lists_controller_spec.rb
+++ spec/controllers/hq/pending_orphan_lists_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Hq::PendingOrphanListsController, type: :controller do
   let(:partner) { build_stubbed :partner }
-  let(:pending_orphan_list) { instance_double PendingOrphanList, spreadsheet: 'sheet', destroy!: true }
+  let(:pending_orphan_list) { instance_double PendingOrphanList, id: 1, spreadsheet: 'sheet', destroy: true, destroy!: true }
   let(:orphan_importer) { double }
   let(:orphan_lists) { double }
   let(:orphan_list) { double }
@@ -13,7 +13,7 @@ RSpec.describe Hq::PendingOrphanListsController, type: :controller do
 
     allow(Partner).to receive(:find).with(partner.id.to_s).and_return partner
     allow(PendingOrphanList).to receive(:new).and_return pending_orphan_list
-    allow(PendingOrphanList).to receive(:find).with('1').and_return pending_orphan_list
+    allow(PendingOrphanList).to receive(:find).with(pending_orphan_list.id.to_s).and_return pending_orphan_list
     allow(OrphanImporter).to receive(:new).and_return orphan_importer
 
     allow(partner).to receive(:orphan_lists).and_return orphan_lists
@@ -120,49 +120,55 @@ RSpec.describe Hq::PendingOrphanListsController, type: :controller do
   end
 
   describe 'import' do
-      let(:orphan) { instance_double Orphan, :save! => true }
-      let(:pending_orphan) { instance_double PendingOrphan, :save! => true }
-      let(:orphans_to_import) { [orphan] }
+    let(:orphan) { instance_double Orphan, :save! => true }
+    let(:pending_orphan) { instance_double PendingOrphan, :save! => true }
+    let(:orphans_to_import) { [orphan] }
 
+    before do
+      allow(orphan_list).to receive :orphan_count=
+      allow(orphan_list).to receive(:orphans).and_return orphans_to_import
+      allow(orphan_list).to receive :save!
+      allow(orphan_list).to receive :orphan_count
+      allow(pending_orphans).to receive(:each).and_yield(pending_orphan)
+      allow(orphan_list).to receive :osra_num
+      allow(pending_orphan_list).to receive(:pending_orphans).and_return pending_orphans
+      allow(pending_orphan).to receive(:to_orphan).and_return orphan
+    end
+
+    context 'when orphan records are valid' do
       before do
-        allow(orphan_list).to receive :orphan_count=
-        allow(orphan_list).to receive(:orphans).and_return orphans_to_import
-        allow(orphan_list).to receive :save!
-        allow(orphan_list).to receive :orphan_count
-        allow(pending_orphan_list).to receive :destroy
-        allow(pending_orphans).to receive(:each).and_yield(pending_orphan)
-        allow(orphan_list).to receive :osra_num
-        allow(pending_orphan_list).to receive(:pending_orphans).and_return pending_orphans
-        allow(pending_orphan).to receive(:to_orphan).and_return orphan
+        allow(orphans_to_import).to receive_message_chain(:map, :all?).and_return true
+        post :import, partner_id: partner.id, orphan_list: { pending_id: 1 }
       end
 
-      context 'when orphan records are valid' do
-        before do
-          allow(orphans_to_import).to receive_message_chain(:map, :all?).and_return true
-          post :import, partner_id: partner.id, orphan_list: { pending_id: 1 }
-        end
-
-        it 'sets instance variables' do
-          expect(assigns :partner).to eq partner
-          expect(assigns :pending_orphan_list).to eq pending_orphan_list
-        end
-
-        it 'saves orphan_list' do
-          expect(orphan_list).to (have_received :save!)
-        end
-
-        it 'destroys pending_orphan_list' do
-          expect(pending_orphan_list).to have_received :destroy
-        end
-
-        it 'sets flash message' do
-          expect(flash[:notice]).to include('was successfully imported.')
-        end
-
-        it 'redirects to partner' do
-          expect(response).to redirect_to hq_partner_path(partner)
-        end
+      it 'sets instance variables' do
+        expect(assigns :partner).to eq partner
+        expect(assigns :pending_orphan_list).to eq pending_orphan_list
       end
+
+      it 'saves orphan_list' do
+        expect(orphan_list).to (have_received :save!)
+      end
+
+      it 'destroys pending_orphan_list' do
+        expect(pending_orphan_list).to have_received :destroy
+      end
+
+      it 'sets flash message' do
+        expect(flash[:notice]).to include('was successfully imported.')
+      end
+
+      it 'redirects to partner' do
+        expect(response).to redirect_to hq_partner_path(partner)
+      end
+    end
   end
 
+  describe 'destroy' do
+    it 'removes existing pending_orphan_list' do
+      delete :destroy, id: pending_orphan_list.id, partner_id: partner.id
+
+      expect(response).to redirect_to hq_partner_path(partner)
+    end
+  end
 end
