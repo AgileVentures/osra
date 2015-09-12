2f620e50460c7ec0330969b3d95cd736ad601330
razor <razorcd@yahoo.com>
2015-09-12 22:42:54 +0300

    Uploading Orphan List for Inactive partner should be impossible

--- spec/features/hq/orphan_list_spec.rb
+++ spec/features/hq/orphan_list_spec.rb
@@ -3,19 +3,34 @@ require 'rails_helper'
 RSpec.feature 'OrphanList', :type => :feature do
 
   background do
-    given_partners_exist [ { name: 'Partner1', status: 'Active', province: 'Damascus & Rif Dimashq' } ]
+    given_partners_exist [ { name: 'PartnerActive', status: 'Active', province: 'Damascus & Rif Dimashq' },
+                           { name: 'PartnerInative', status: 'Inactive', province: 'Damascus & Rif Dimashq' } ]
     i_sign_in_as_admin
   end
 
-  scenario 'Upload valid orphan_list' do
-    visit_partner 'Partner1'
+  scenario "Uploading Orphan List for Active partner should be possible" do
+    visit_partner 'PartnerActive'
     and_i_should_see_link "Upload Orphan List"
     and_i_click_link "Upload Orphan List"
-    and_i_should_be_on :upload_hq_partner_pending_orphan_lists, { partner_name: 'Partner1' }
+    and_i_should_be_on :upload_hq_partner_pending_orphan_lists, { partner_name: 'PartnerActive' }
+  end
+
+  scenario "Uploading Orphan List for Inactive partner should be impossible" do
+    visit_partner 'PartnerInative'
+    and_i_should_see_link "Upload Orphan List"
+    and_i_click_link "Upload Orphan List"
+    and_i_should_be_on :hq_partner_page, { partner_name: 'PartnerInative' }
+    and_i_should_see "Partner is not Active. Orphan List cannot be uploaded."
+  end
+
+  scenario 'Upload valid orphan_list for an active partner' do
+    visit_partner 'PartnerActive'
+    and_i_click_link "Upload Orphan List"
+    and_i_should_be_on :upload_hq_partner_pending_orphan_lists, { partner_name: 'PartnerActive' }
     and_i_upload_valid_orphan_list
     and_i_should_see "Orphan list is valid"
     and_i_click_button "Import"
-    and_i_should_be_on :hq_partner_page, { partner_name: 'Partner1' }
+    and_i_should_be_on :hq_partner_page, { partner_name: 'PartnerActive' }
     and_i_should_see "Orphan List (0001) was successfully imported. Registered 3 new orphans."
   end
 
@@ -30,7 +45,7 @@ RSpec.feature 'OrphanList', :type => :feature do
   def and_i_upload_valid_orphan_list
     attach_file 'pending_orphan_list_spreadsheet', "spec/fixtures/three_orphans_xlsx.xlsx"
     and_i_click_button "Upload"
-    and_i_should_be_on :validate_hq_partner_pending_orphan_lists, { partner_name: 'Partner1' }
+    and_i_should_be_on :validate_hq_partner_pending_orphan_lists, { partner_name: 'PartnerActive' }
   end
 
 end
