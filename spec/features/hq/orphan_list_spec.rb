4a5cbd1e1c3a0923e9a65ae603dbeeaf39d30b6c
razor <razorcd@yahoo.com>
2015-09-12 22:26:53 +0300

    feature importing valid orphanlists

--- spec/features/hq/orphan_list_spec.rb
+++ spec/features/hq/orphan_list_spec.rb
@@ -12,10 +12,11 @@ RSpec.feature 'OrphanList', :type => :feature do
     and_i_should_see_link "Upload Orphan List"
     and_i_click_link "Upload Orphan List"
     and_i_should_be_on :upload_hq_partner_pending_orphan_lists, { partner_name: 'Partner1' }
-    attach_file 'pending_orphan_list_spreadsheet', "spec/fixtures/three_orphans_xlsx.xlsx"
-    and_i_click_button "Upload"
-    and_i_should_be_on :validate_hq_partner_pending_orphan_lists, { partner_name: 'Partner1' }
+    and_i_upload_valid_orphan_list
     and_i_should_see "Orphan list is valid"
+    and_i_click_button "Import"
+    and_i_should_be_on :hq_partner_page, { partner_name: 'Partner1' }
+    and_i_should_see "Orphan List (0001) was successfully imported. Registered 3 new orphans."
   end
 
 
@@ -26,6 +27,11 @@ RSpec.feature 'OrphanList', :type => :feature do
     and_i_should_be_on :hq_partner_page, { partner_name: partner_name }
   end
 
+  def and_i_upload_valid_orphan_list
+    attach_file 'pending_orphan_list_spreadsheet', "spec/fixtures/three_orphans_xlsx.xlsx"
+    and_i_click_button "Upload"
+    and_i_should_be_on :validate_hq_partner_pending_orphan_lists, { partner_name: 'Partner1' }
+  end
 
 end
 
