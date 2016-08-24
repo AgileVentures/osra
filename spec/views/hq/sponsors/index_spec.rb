4483b44e511968aba8a45c9da7b0cbd624fec87a
develaper <lachguer.brahim@gmail.com>
2016-08-24 16:49:06 +0200

    Add total number of sponsors in index

--- spec/views/hq/sponsors/index_spec.rb
+++ spec/views/hq/sponsors/index_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'will_paginate/array'
 
 RSpec.describe 'hq/sponsors/index.html.haml', type: :view do
   before :each do
@@ -7,6 +8,13 @@ RSpec.describe 'hq/sponsors/index.html.haml', type: :view do
     assign(:sort_by, {})
     assign(:sortable_by_column, true)
   end
+
+  let(:sponsors_count) { build_stubbed_list(:sponsor, 5).count }
+  let(:sponsors) do
+    items = build_stubbed_list :sponsor, 5
+    items.paginate(:page => 2, :per_page => 2)
+  end
+
   it 'should delegate to partial' do
     render
 
@@ -23,6 +31,13 @@ RSpec.describe 'hq/sponsors/index.html.haml', type: :view do
     render
 
     expect(rendered).to have_text 'Currently sponsoring 3 orphans out of 10 requested.'
+  end
+
+  it 'should show total number of sponsors' do
+    assign(:sponsors_count, sponsors_count)
+    assign(:sponsors, sponsors)
+    render
+    expect(rendered).to have_content('Displaying ' + sponsors.count.to_s + ' of ' + sponsors_count.to_s + ' Sponsors.')
   end
 
   describe 'class action-items should have link' do
