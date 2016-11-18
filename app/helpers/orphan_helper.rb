ba063bc9a9613f22fabab699896be777b563d863
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2016-11-18 16:12:57 +0200

    Change age comparison to include AGE_OF_ADULTHOOD itself

--- app/helpers/orphan_helper.rb
+++ app/helpers/orphan_helper.rb
@@ -1,5 +1,5 @@
 module OrphanHelper
   def child_or_adult?(orphan)
-    orphan.age_in_years > Orphan::AGE_OF_ADULTHOOD ? "adult" : "child"
+    orphan.age_in_years >= Orphan::AGE_OF_ADULTHOOD ? "adult" : "child"
   end
 end
