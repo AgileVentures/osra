fc8927f8202df448a289ca3658787e669f077a6f
Marouen Bousnina <marouen.it.technology@gmail.com>
2016-12-05 09:38:54 +0100

    change will_paginate helper method

--- app/helpers/will_paginate_helper.rb
+++ app/helpers/will_paginate_helper.rb
@@ -1,15 +1,11 @@
 module WillPaginateHelper
 
-  def will_paginate_render_options
+  def will_paginate_render_options(options = {})
     {
       inner_window: 2,
       outer_window: 0,
       renderer: BootstrapPagination::Rails
-    }
-  end
-
-  def render_options_with_custom_param(param_name)
-    will_paginate_render_options.merge(param_name: param_name)
+    }.merge(options)
   end
 
 end
