86b3eb8d3faca0e0c024ba620eaf8b4cd3d8fedf
b264 <public88878878887@hotmail.com>
2016-12-18 04:16:13 +0000

    refactor Halalas

--- app/helpers/halalas.rb
+++ app/helpers/halalas.rb
@@ -1,26 +1,24 @@
 # coding: utf-8
 # frozen_string_literal: true
 class Halalas
+  CURRENCY= ' SAR'
+
   def initialize value
-    @value= Integer(value)
-    @currency_symbol= ' SAR'
+    @halalas= Integer(value)
+    @riyals= Rational(@halalas, 100)
   end
 
   def to_s
-    riyals_string + @currency_symbol
+    riyals_string + CURRENCY
   end
 
 private
 
-  def riyals
-    @value.to_r / 100
-  end
-
   def riyals_string
-    if riyals < 0
-      ('(%.2f)' % riyals.abs)
+    if @riyals < 0
+      '(%.2f)' % @riyals.abs
     else
-      ('%.2f' % riyals)
+      '%.2f' % @riyals
     end
   end
 end
