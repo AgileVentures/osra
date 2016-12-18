0b60552cc3f9f8361c2ea39144518aaded58ae47
b264 <public88878878887@hotmail.com>
2016-12-18 18:42:28 +0000

    prune redundant spec

--- spec/helpers/halala_spec.rb
+++ spec/helpers/halala_spec.rb
@@ -5,11 +5,7 @@ require 'rails_helper'
 RSpec.describe Halala, type: :helper do
 
   specify 'requires integer-like input' do
-    good_inputs= [123, -1234, 0, '123', '0', '-123']
     bad_inputs= [true, false, nil, 'foo', '6.0e7', '﷼']
-    good_inputs.each do |input|
-      expect{Halala.new(input)}.to_not raise_error
-    end
     bad_inputs.each do |input|
       expect{Halala.new(input)}.to raise_error
     end
@@ -19,9 +15,9 @@ RSpec.describe Halala, type: :helper do
     expect(Halala.new(123).to_s).to eq '1.23 SAR'
     expect(Halala.new(0).to_s).to eq '0.00 SAR'
     expect(Halala.new('-0').to_s).to eq '0.00 SAR'
-    expect(Halala.new(-1234).to_s).to eq '(12.34) SAR'
-    expect(Halala.new('1050').to_s).to eq '10.50 SAR'
-    expect(Halala.new(20000).to_s).to eq '200.00 SAR'
+    expect("#{Halala.new(-1234)}").to eq '(12.34) SAR'
+    expect("#{Halala.new('1050')}").to eq '10.50 SAR'
+    expect("#{Halala.new(20000)}").to eq '200.00 SAR'
   end
 end
 
