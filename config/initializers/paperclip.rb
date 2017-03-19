a43b5fe90d0c61d14f3d750e3140f1f05c118287
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2017-03-19 13:09:48 +0200

    Replace s3_host_domain config with url & path config for Paperclip

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,5 @@
-Paperclip::Attachment.default_options[:s3_host_name] = ENV["S3_HOST_NAME"] || "s3.amazonaws.com"
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 
 module Paperclip
   class MediaTypeSpoofDetector
