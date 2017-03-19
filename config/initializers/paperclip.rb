bdb637775255a41b8dbf606fdd199f80a92b8c09
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2017-03-19 13:34:36 +0200

    TEST: Change Paperclip path

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,5 @@
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:url] = ":s3_domain_url"
+Paperclip::Attachment.default_options[:path] = ":rails_root/public/system/:class/:attachment/:id_partition/:style/:filename"
 
 module Paperclip
   class MediaTypeSpoofDetector
