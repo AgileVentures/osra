8b4045c304b939f90c2ed41b27b75b8a1696e8f3
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2017-03-19 13:41:19 +0200

    Configure Paperclip path to use public/system in test env

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,13 @@
+# frozen_string_literal: true
+
 Paperclip::Attachment.default_options[:url] = ":s3_domain_url"
-Paperclip::Attachment.default_options[:path] = ":rails_root/public/system/:class/:attachment/:id_partition/:style/:filename"
+if Rails.env == "test"
+  Paperclip::Attachment.default_options[:path] =
+    ":rails_root/public/system/:class/:attachment/:id_partition/:style/:filename"
+else
+  Paperclip::Attachment.default_options[:path] =
+    "/:class/:attachment/:id_partition/:style/:filename"
+end
 
 module Paperclip
   class MediaTypeSpoofDetector
