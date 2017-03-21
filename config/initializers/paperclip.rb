bb2d84cd14ca8d38955acc39eeb7321c570bd937
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2017-03-21 15:37:16 +0200

    Use public/system for Paperclip except in production

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 Paperclip::Attachment.default_options[:url] = ":s3_domain_url"
-if Rails.env == "test" || ENV["CI"]
-  Paperclip::Attachment.default_options[:path] =
-    ":rails_root/public/system/:class/:attachment/:id_partition/:style/:filename"
-else
+if Rails.env == "production"
   Paperclip::Attachment.default_options[:path] =
     "/:class/:attachment/:id_partition/:style/:filename"
+else
+  Paperclip::Attachment.default_options[:path] =
+    ":rails_root/public/system/:class/:attachment/:id_partition/:style/:filename"
 end
 
 module Paperclip
